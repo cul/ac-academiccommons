fb1add5c99feb325893aa67fe081ce1b865d556b
James Stuart <tastyhat@jamesstuart.org>
2010-08-26 17:05:27 -0400

    cleaned up environments and deploy scripts
    removed ludwig_prod and pass_test, modified deploy scripts to point
    to the right application, tweaked deploy.rb

--- config/deploy/taft_pass.rb
+++ config/deploy/taft_pass.rb
@@ -1,11 +1,14 @@
-set :rails_env, "pass_test"
+set :rails_env, "taft_pass"
+set :application, "ac2_test"
 set :domain,      "taft.cul.columbia.edu"
-set :deploy_to,   "/opt/passenger/ac2_test/"
+set :deploy_to,   "/opt/passenger/#{application}/"
 set :user, "deployer"
-set :branch, "pass_test"
+set :branch, @variables[:branch] || "taft_pass_test"
 set :scm_passphrase, "Current user can full owner domains."
 
 role :app, domain
 role :web, domain
 role :db,  domain, :primary => true
+
+
 
