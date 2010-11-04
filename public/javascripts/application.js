d3b6f2d1f091286d401f9821398d10b166965119
Patrick Force <pbf2105@dyn-iab-165-101.dyn.columbia.edu>
2010-11-04 16:41:37 -0400

    Adding search results term highlighting

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,7 +11,26 @@ $(document).ready(function() {
   $('table.zebra tr:even').addClass('zebra_stripe');
   $('ul.zebra li:even').addClass('zebra_stripe');
  
-
+  //add highlight to search results
+  var q = $("#q");
+  if(q != null && q != undefined && q != "")
+  {
+	  q = q.val();
+	  $(".document *").each
+	  (
+		  function()
+		  {
+			  console.debug($(this));
+			  if($(this).children().length == 0)
+		      {
+				  var re = new RegExp(q, "g");
+				  console.debug($(this).text());
+				  $(this).html($(this).text().replace(re, '<span class="highlight">' + q + '</span>'));
+		      }
+		  }
+	  );
+  }
+  
 /*************  
  * Facet more dialog. Uses JQuery UI Dialog. Use crazy closure technique. 
  * http://docs.jquery.com/UI/Dialog
