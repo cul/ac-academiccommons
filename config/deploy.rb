4f81bf304145ca60f67eeee9741786c372528474
James Stuart <tastyhat@jamesstuart.org>
2010-12-23 11:46:14 -0500

    added taft_test and taft_dev enviroments

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-set :default_stage, "taft_test taft_dev"
-set :stages, %w(taft_test)
+set :default_stage, "taft_dev"
+set :stages, %w(taft_test taft_dev)
 
 require 'capistrano/ext/multistage'
 default_run_options[:pty] = true
