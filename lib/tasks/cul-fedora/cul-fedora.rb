93e89f2c2f28192a7d2dc8802926a7ef30634cfc
rockholla <patrickforce@gmail.com>
2011-04-28 00:36:50 -0400

    adding back in temp cul-fedora gem for rake reindexing

---
+++ lib/tasks/cul-fedora/cul-fedora.rb
@@ -0,0 +1,11 @@
+require "httpclient"
+require "nokogiri"
+require "activesupport"
+require "rsolr"
+require "rsolr-ext"
+require "open3"
+
+
+require "cul-fedora/item"
+require "cul-fedora/server"
+require "cul-fedora/solr"
