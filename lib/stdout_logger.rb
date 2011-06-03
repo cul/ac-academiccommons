a33828d955c22baf0712d79589a083c043f219cb
rockholla <patrickforce@gmail.com>
2011-06-03 12:56:22 -0400

    Making sure stdout doesn't output any level logger shouldn't be outputting

--- lib/stdout_logger.rb
+++ lib/stdout_logger.rb
@@ -11,6 +11,9 @@ class StdOutLogger < Logger
 
   def add(severity, message = nil, progname = nil, &block)
     severity ||= UNKNOWN
+    if @logdev.nil? or severity < @level
+      return true
+    end
     progname ||= @progname
     if message.nil?
       if block_given?
