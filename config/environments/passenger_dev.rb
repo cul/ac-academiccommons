bab8336e1e11dcc435104b88054049ca45422693
James Stuart <github@jamesstuart.org>
2011-06-14 16:13:52 -0400

    fixed bug in dev environment

--- config/environments/passenger_dev.rb
+++ config/environments/passenger_dev.rb
@@ -18,10 +18,9 @@ config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address => "localhost",
-  :domain => "cockle.cc.columbia.edu",
+  :domain => "rowling.cc.columbia.edu",
   :port => 25
 }
-~          
 
 config.action_controller.relative_url_root = "/ac2_dev"
 
