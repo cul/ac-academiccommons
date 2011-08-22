c5d2ba7897ef342315fb383518b9908b9ea04c05
rockholla <patrickforce@gmail.com>
2011-08-22 18:06:33 -0400

    finished file uploading and making available via URL

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -1,8 +1,8 @@
 class UploadedFile < ActiveRecord::Base
   
-  def self.save(uploaded_file, as_name)
-    directory = "data/uploads"
+  def self.save(uploaded_file, directory, as_name)
     path = File.join(directory, as_name)
+    FileUtils.mkdir_p directory
     FileUtils.mv uploaded_file.path, path
   end
   
