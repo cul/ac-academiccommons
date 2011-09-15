7a60e5a865905210a2127e51577e5a2331da94d6
rockholla <patrickforce@gmail.com>
2011-09-15 11:19:47 -0400

    adding deploy files

---
+++ config/deploy/passenger_prod.rb
@@ -0,0 +1,11 @@
+set :rails_env, "passenger_prod"
+set :application, "ac2"
+set :domain,      "brahms.cul.columbia.edu"
+set :deploy_to,   "/opt/passenger/#{application}/"
+set :user, "deployer"
+set :branch, @variables[:branch] || "passenger_prod"
+set :scm_passphrase, "Current user can full owner domains."
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
