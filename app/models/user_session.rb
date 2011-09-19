94da11cf08b57837c50be014f49d2423602105bc
rockholla <patrickforce@gmail.com>
2011-09-19 13:34:11 -0400

    removing all for recreation from current branch

--- app/models/user_session.rb
+++
@@ -1,8 +0,0 @@
-class UserSession < Authlogic::Session::Base 
-  unloadable
-  
-  wind_host "wind.columbia.edu"
-  wind_service "cul_headcount"
-  auto_register true
-  login_only_with_wind true
-end
--- lib/pagevisits.rb
+++
@@ -1,6 +0,0 @@
-require "garb"
-
-class Pagevisits
-  extend Garb::Model
-  metrics :visits
-end
