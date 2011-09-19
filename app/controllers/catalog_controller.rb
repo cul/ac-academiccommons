6ab91adef8e5be051ddcf1f1a1627642df84acaf
rockholla <patrickforce@gmail.com>
2011-09-19 13:29:41 -0400

    cleaning out master to replace w/ new content

--- app/controllers/catalog_controller.rb
+++
@@ -1,5 +0,0 @@
-class CatalogController < ApplicationController
-
-  unloadable
-  # before_filter :require_user
-end
--- lib/blacklight/solr.rb
+++
@@ -1,7 +0,0 @@
-module Blacklight::Solr
-  
-  autoload :Document, 'blacklight/solr/document'
-  autoload :FacetPaginator, 'blacklight/solr/facet_paginator'
-  require 'blacklight/rsolr_facets'
-
-end
