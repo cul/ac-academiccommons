d304a202305108bb20b76028c7f79a5bd1faa4ef
Aleksey Pokalyukhin <ap2972@dyn-iab-164-131.dyn.columbia.edu>
2012-06-19 11:40:43 -0400

    (AC-464)

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,2 +1,24 @@
+require 'csv'
+
 module StatisticsHelper
+   
+  def cvsReport(results, stats, totals)
+    
+    csv = CSV.generate_line(["Total for " + params[:month].to_s + " " + params[:year].to_s, 
+                            "", 
+                            totals["View"].to_s, 
+                            totals["Download"].to_s]) + "\r\n"
+                            
+    csv += CSV.generate_line(["Title", "Link",  "Views", "Downloads"]) + "\r\n"
+
+    results.each do |item|
+      csv += CSV.generate_line([item["title_display"],
+                              "http://" + request.host_with_port + catalog_path(item["id"]),
+                              stats["View"][item["id"][0]],
+                              stats["Download"][item["id"][0]]]) + "\r\n"
+    end
+
+    return csv
+  end
+
 end
