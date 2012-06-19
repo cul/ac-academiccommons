c80a76efc9e405d30e8de2314afc4e6cbdccb6c3
Aleksey Pokalyukhin <ap2972@dyn-iab-164-131.dyn.columbia.edu>
2012-06-19 14:22:01 -0400

    (AC-464) fixed missed zeroes for empty view results

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -4,6 +4,8 @@ module StatisticsHelper
    
   def cvsReport(results, stats, totals)
     
+    #  + "\r\n" after CSV.generate_line is only for ruby version 1.8.7, with version 1.9.3 it should be removed
+    
     csv = "Author UNI: " + params[:author_id].to_s + "\r\n"
     csv += CSV.generate_line(["Total for " + params[:month].to_s + " " + params[:year].to_s, 
                             "", 
@@ -15,8 +17,9 @@ module StatisticsHelper
     results.each do |item|
       csv += CSV.generate_line([item["title_display"],
                               "http://" + request.host_with_port + catalog_path(item["id"]),
-                              stats["View"][item["id"][0]],
-                              stats["Download"][item["id"][0]]]) + "\r\n"
+                              stats["View"][item["id"][0]].nil? ? 0 : stats["View"][item["id"][0]],
+                              stats["Download"][item["id"][0]].nil? ? 0 : stats["Download"][item["id"][0]]
+                              ]) + "\r\n"
     end
 
     return csv
