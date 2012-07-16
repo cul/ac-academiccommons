3410eac1d301ab0f3b44124e5109d655616e66b0
Aleksey Pokalyukhin <ap2972@ltltmcdrs02.cul.columbia.edu>
2012-07-16 10:56:28 -0400

    removed asset pipeline statements from top of files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,10 +2,6 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 
-//= require jquery
-//= require jquery_ujs
-//= require_self
-//= require_tree .
 
 $(document).ready(function() {
 
