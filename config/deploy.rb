6fcd8ac082e235c2ddd2c8c06881fbbd1f9d21d3
JackBlackLight <jd2148@columbia.edu>
2013-04-26 15:25:07 -0400

    added new deploy stage to deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :default_stage, "passenger_bl4_dev"
-set :stages, %w(passenger_dev passenger_test passenger_prod passenger_bl4_dev passenger_bl4_test)
+set :stages, %w(passenger_dev passenger_test passenger_prod passenger_bl4_dev passenger_bl4_test ac2_bl4_dev)
 
 require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
