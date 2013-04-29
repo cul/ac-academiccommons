371be245b8f0cb63ce068537d2f2cd570ca02293
Aleksey Pokalyukhin <ap2972@dyn-209-2-230-247.dyn.columbia.edu>
2013-04-29 16:27:32 -0400

    poking around broken pipeline

--- config/application.rb
+++ config/application.rb
@@ -78,6 +78,8 @@ module CulBlacklightAc2
     # Enable the asset pipeline
     config.assets.enabled = true
     
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
  # =============== new end ==================== #   
--- config/environments/ac2_bl4_dev.rb
+++ config/environments/ac2_bl4_dev.rb
@@ -60,7 +60,7 @@ CulBlacklightAc2::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
-
+  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
   # Generate digests for assets URLs
   config.assets.digest = true
 
