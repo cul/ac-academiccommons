8990b580ad34d54cade6d42f0af8bf8aabf67306
Aleksey Pokalyukhin <ap2972@dyn-209-2-213-77.dyn.columbia.edu>
2013-05-20 14:44:25 -0400

    changed deploy tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :default_stage, "passenger_bl4_dev"
-set :stages, %w(passenger_dev passenger_test passenger_prod passenger_bl4_dev passenger_bl4_test ac2_bl4_dev)
+set :stages, %w(passenger_dev passenger_test passenger_prod ac2_bl4_dev ac2_bl4_test)
 
 require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
