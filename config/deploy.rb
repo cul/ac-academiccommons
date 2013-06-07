cd075370b53471ab506b47ab89806110cb7ad7d8
Aleksey Pokalyukhin <ap2972@dyn-160-39-10-174.dyn.columbia.edu>
2013-06-07 12:32:14 -0400

    added deployment commit for ac2_prod

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :default_stage, "passenger_bl4_dev"
-set :stages, %w(passenger_dev passenger_test passenger_prod ac2_bl4_dev ac2_bl4_test)
+set :stages, %w(passenger_dev passenger_test passenger_prod ac2_bl4_dev ac2_bl4_test ac2_prod)
 
 require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
---
+++ config/deploy/ac2_prod.rb
@@ -0,0 +1,12 @@
+set :rails_env, "ac2_prod"
+set :application, "ac2_prod"
+set :domain,      "bernstein.cul.columbia.edu"
+set :deploy_to,   "/opt/passenger/#{application}/"
+set :user, "deployer"
+set :branch, @variables[:branch] || "ac2_prod"
+set :default_branch, "ac2_prod"
+set :scm_passphrase, "Current user can full owner domains."
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
