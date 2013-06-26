5d3145939b9b4b6f932a667c33be3c4557f63194
Rob Hilliker <rhilliker@columbia.edu>
2013-06-26 14:17:53 -0400

    Update test mail config

--- config/environments/ac2_bl4_test.rb
+++ config/environments/ac2_bl4_test.rb
@@ -42,8 +42,8 @@ CulBlacklightAc2::Application.configure do
   config.relative_root = ''
   config.analytics_enabled = false
   
-  config.mail_deposit_recipients = ["rh2561@columbia.edu", "bal35@columbia.edu"]
-  config.mail_deliverer = "rh2561@columbia.edu"
+  config.mail_deposit_recipients = ["cuac@libraries.cul.columbia.edu"]
+  config.mail_deliverer = "cuac@libraries.cul.columbia.edu"
   config.base_path = "berlioz.cul.columbia.edu"
 
 
