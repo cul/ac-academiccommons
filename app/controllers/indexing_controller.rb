150fc9198865d2eb3dd453154e3aaa7c2857de54
Aleksey Pokalyukhin <alpok45@catsads.com>
2014-10-29 16:47:56 -0400

    AC-1553 'push_indexing_results' log format changed

--- app/controllers/indexing_controller.rb
+++ app/controllers/indexing_controller.rb
@@ -10,20 +10,39 @@ class IndexingController < ApplicationController
     
     reindexed = params[:reindexed] == nil ? [] : params[:reindexed].split(',')
     new_indexed = params[:new_indexed] == nil ? [] : params[:new_indexed].split(',')
+    
+    embargo_new = params[:embargo_new] == nil ? [] : params[:embargo_new].split(',')
+    embargo_reindexed = params[:embargo_reindexed] == nil ? [] : params[:embargo_reindexed].split(',')
+    embargo_new_released = params[:embargo_new_released] == nil ? [] : params[:embargo_new_released].split(',')
+    embargo_released_reindexed = params[:embargo_released_reindexed] == nil ? [] : params[:embargo_released_reindexed].split(',')
+    
     failed = params[:failed] == nil ? [] : params[:failed].split(',')
     
     logger.info ''
-    logger.info '   started: ' + (params[:started] == nil ? '' :  params[:started])
-    logger.info '  finished: ' + (params[:finished] == nil ? '' :  params[:finished])
-    logger.info 'time spent: ' + (params[:time_spent] == nil ? '' :  params[:time_spent])
+    logger.info '                     started: ' + (params[:started] == nil ? '' :  params[:started])
+    logger.info '                    finished: ' + (params[:finished] == nil ? '' :  params[:finished])
+    logger.info '                  time spent: ' + (params[:time_spent] == nil ? '' :  params[:time_spent])
     logger.info ''
-    logger.info '  reindexed (' + reindexed.size.to_s + ') - ' + (params[:reindexed] == nil ? '' :  params[:reindexed])
-    logger.info 'new indexed (' + new_indexed.size.to_s + ') - ' + (params[:new_indexed] == nil ? '' :  params[:new_indexed])
-    logger.info '     failed (' + failed.size.to_s + ') - ' + (params[:failed] == nil ? '' :  params[:failed])
-    
+    logger.info '               all processed: '  + params[:all_processed]
+    logger.info ''
+    logger.info '                 new indexed (' + new_indexed.size.to_s + ') - ' + (params[:new_indexed] == nil ? '' :  params[:new_indexed])
+    logger.info '                   reindexed (' + reindexed.size.to_s + ') - ' + (params[:reindexed] == nil ? '' :  params[:reindexed])
+    logger.info ''
+    logger.info '               new embargoed (' + embargo_new.size.to_s + ') - ' + (params[:embargo_new] == nil ? '' :  params[:embargo_new])
+    logger.info '         embargoed reindexed (' + embargo_reindexed.size.to_s + ') - ' + (params[:embargo_reindexed] == nil ? '' :  params[:embargo_reindexed])
+    logger.info '      new embargoed released (' + embargo_new_released.size.to_s + ') - ' + (params[:embargo_new_released] == nil ? '' :  params[:embargo_new_released])
+    logger.info 'embargoed released reindexed (' + embargo_released_reindexed.size.to_s + ') - ' + (params[:embargo_released_reindexed] == nil ? '' :  params[:embargo_released_reindexed])
+    logger.info ''
+    logger.info '                      failed (' + failed.size.to_s + ') - ' + (params[:failed] == nil ? '' :  params[:failed])
+    logger.info ''
+
     
     if(new_indexed.size > 0)
       notifyDepositorsItemAdded(new_indexed)
+    end
+    
+    if(embargo_new_released.size > 0)
+      notifyDepositorsItemAdded(embargo_new_released)
     end
 
     render nothing: true 
