d18eb8f2357c4f92377a992e03175e1b084810c0
Aleksey Pokalyukhin <alpok45@catsads.com>
2014-11-21 11:27:40 -0500

    fixed problem with occasionally received multi author's ids as string from solr

--- app/helpers/solr_helper.rb
+++ app/helpers/solr_helper.rb
@@ -17,14 +17,26 @@ module SolrHelper
       item.handle = result.first[:handle]
       item.free_to_read_start_date = result.first[:free_to_read_start_date]
       
-      if(result.first[:author_uni] == nil)
-        item.authors_uni = [] # this is duplicated in else
-      else
-        item.authors_uni = result.first[:author_uni] || []
+      item.authors_uni = []
+      
+      if(result.first[:author_uni] != nil)
+        # item.authors_uni = result.first[:author_uni] || []
+        item.authors_uni = fixAuthorsArray(result.first[:author_uni])
       end  
       
     return item
   end    
+  
+  def fixAuthorsArray(authors_uni)
+    
+    author_unis_clean = []
+    
+    authors_uni.each do | uni_str |
+      author_unis_clean.push(uni_str.split(', '))
+    end
+    
+    return author_unis_clean.flatten
+  end
   
 
 end # =============================================================== #
