e3e29b3189b755f97f2c862f73b9a2d6b776dc1e
Megan O'Neill <mo2638@columbia.edu>
2015-11-19 10:41:46 -0500

    add required fields

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,7 +10,7 @@ class Message < MailForm::Base
 
 
 
-  validates :email, :presence => true
+  validates :email, :first_name, :last_name, :intl_prop, :kind, :url, :infringe_url, :evidence, :signature, :presence => true
   validates :email, :format => { :with => %r{.+@.+\..+} }, :allow_blank => false
 
   
