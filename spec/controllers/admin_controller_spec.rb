42e60176818502e8e376a1b5e8a3a80e1e795b26
Benjamin Armintor <armintor@gmail.com>
2016-06-02 06:51:13 -0400

    spec authz checks on AdminController show methods

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -42,4 +42,76 @@ describe AdminController, :type => :controller do
       end
     end
   end
+  [:show_deposit, :download_deposit_file].each do |action|
+    describe action do
+      before do
+        @deposit = double(Deposit)
+        @params = {:id => 'foo'}
+        allow(Deposit).to receive(:find).with(@params[:id]).and_return(@deposit)
+        this_file = __FILE__.sub(Rails.root.to_s + '/','')
+        allow(@deposit).to receive(:file_path).and_return(this_file)
+      end
+      context "without being logged in" do
+        before do
+          allow(controller).to receive(:current_user).and_return(nil)
+        end
+        it "redirects to new_user_session_path" do
+          get action, @params
+          expect(response.status).to eql(302)
+          expect(response.headers['Location']).to eql(new_user_session_url)
+        end
+      end
+      context "logged in as a non-admin user" do
+        before do
+          allow(controller).to receive(:current_user).and_return(@non_admin)
+        end
+        it "fails" do
+          get action, @params
+          expect(response.status).to eql(302)
+          expect(response.headers['Location']).to eql(access_denied_url)
+        end
+      end
+      context "logged in as an admin user" do
+        before do
+          allow(controller).to receive(:current_user).and_return(@admin)
+        end
+        it "succeeds" do
+          get action, @params
+          expect(response.status).to eql(200)
+        end
+      end
+    end
+    describe "ingest" do
+      context "without being logged in" do
+        before do
+          allow(controller).to receive(:current_user).and_return(nil)
+        end
+        it "redirects to new_user_session_path" do
+          get :ingest, @params
+          expect(response.status).to eql(302)
+          expect(response.headers['Location']).to eql(new_user_session_url)
+        end
+      end
+      context "logged in as a non-admin user" do
+        before do
+          allow(controller).to receive(:current_user).and_return(@non_admin)
+        end
+        it "fails" do
+          get :ingest, @params
+          expect(response.status).to eql(302)
+          expect(response.headers['Location']).to eql(access_denied_url)
+        end
+      end
+      context "logged in as an admin user" do
+        before do
+          allow(controller).to receive(:current_user).and_return(@admin)
+          expect(controller).to receive(:processIndexing)
+        end
+        it "succeeds" do
+          get :ingest, @params
+          expect(response.status).to eql(200)
+        end
+      end
+    end
+  end
 end
