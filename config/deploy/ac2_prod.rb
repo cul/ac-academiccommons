a85a3929920e21c95ebd7d12f932828e9cfdb915
Benjamin Armintor <armintor@gmail.com>
2016-08-31 16:37:59 -0400

    port cap configs forward unset is delete in cap3 use fetch instead of bare variable names for cap variables

--- config/deploy/ac2_prod.rb
+++ config/deploy/ac2_prod.rb
@@ -1,13 +1,13 @@
-set :rails_env, "ac2_prod"
+# on bernstein, there is no RVM
+delete :rvm_ruby_version
+# on bernstein, ac2_prod and passenger_prod are identical; latter is in nginx.conf
+# and there is no ac2_prod Rails config
+set :rails_env, "passenger_prod"
 set :application, "ac2"
-set :domain,      "bernstein.cul.columbia.edu"
-set :deploy_to,   "/opt/passenger/#{application}/"
-#set :deploy_to,   "/opt/passenger/ac2/"
-set :user, "deployer"
-set :branch, @variables[:branch] || "master"
+server 'bernstein.cul.columbia.edu', user: fetch(:remote_user), roles: %w(app db web)
+# override deploy_to
+set :deploy_to,   "/opt/passenger/#{fetch(:application)}/"
+set :remote_user, "deployer"
+ask :branch, proc { `git tag --sort=version:refname`.split("\n").last }
 set :default_branch, "master"
 set :scm_passphrase, "Current user can full owner domains."
-
-role :app, domain
-role :web, domain
-role :db,  domain, :primary => true
