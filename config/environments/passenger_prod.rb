aead41cc86974ee5778a2eb096d89a9719690e47
Benjamin Armintor <armintor@gmail.com>
2016-09-01 13:12:27 -0400

    rake and environments all refer to correct application name

--- config/environments/passenger_prod.rb
+++ config/environments/passenger_prod.rb
@@ -1,4 +1,4 @@
-CulBlacklightAc2::Application.configure do
+AcademicCommons::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
--- config/environments/passenger_test.rb
+++ config/environments/passenger_test.rb
@@ -1,4 +1,4 @@
-CulBlacklightAc2::Application.configure do
+AcademicCommons::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
