c3718b08bbc478b417186cf172f94cf164e0ff6b
Carla Galarza <cmg2228@columbia.edu>
2016-10-10 10:14:41 -0400

    Simplified is_bot? method

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.describe ApplicationController, :type => :controller do
+
+  describe '#is_bot?' do
+    context 'return true' do
+      it 'when user agent is a bot' do
+        expect(controller.is_bot?('yahoo')).to be true
+      end
+    end
+
+    context 'returns false' do
+      it 'when user agent is not a bot' do
+        expect(controller.is_bot?('columbia')).to be false
+      end
+
+      it 'when user agent name is empty' do
+        expect(controller.is_bot? '').to be false
+      end
+    end
+  end
+end
