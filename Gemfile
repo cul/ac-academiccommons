9d45b06fe6d46d9126ab32c6e6e4b268246cd735
Carla Galarza <cmg2228@columbia.edu>
2016-10-11 11:53:54 -0400

    Removed assets gem group.

--- Gemfile
+++ Gemfile
@@ -42,13 +42,9 @@ gem 'font-awesome-rails'
 # TODO: Confirm that this is needed and used by the application.
 gem 'fancybox2-rails'
 
-# Gems used only for assets and not required
-# in production environments by default.
-group :assets do
-  gem 'sass-rails', " ~> 3.2.4"
-  gem 'coffee-rails', " ~> 3.2.2"
-  gem 'uglifier'
-end
+gem 'sass-rails', " ~> 3.2.4"
+gem 'coffee-rails', " ~> 3.2.2"
+gem 'uglifier'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- config/application.rb
+++ config/application.rb
@@ -2,22 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# If you have a Gemfile, require the gems listed there, including any gems
+# Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-
-# old
-#Bundler.require(:default, Rails.env) if defined?(Bundler)
-
-# =============== new start =================== #
-
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
-
-# =============== new end ==================== #
+Bundler.require(*Rails.groups)
 
 module AcademicCommons
   class Application < Rails::Application
