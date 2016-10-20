2f5e03600a7b5e111b3c133725683604655eee67
Carla Galarza <cmg2228@columbia.edu>
2016-10-20 09:29:03 -0400

    Log download restricted to administrators.

---
+++ spec/controllers/download_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+RSpec.describe DownloadController, :type => :controller do
+  describe 'GET download_log' do
+    include_context 'log'
+
+   include_examples 'authorization required' do
+     let(:request) { get :download_log, :log_folder => 'ac-indexing', :id => id }
+   end
+  end
+end
