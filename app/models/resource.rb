11d080be91842c6642b2f97e8327a5c190f4c3c3
Benjamin Armintor <armintor@gmail.com>
2016-10-26 16:06:03 -0400

    add a Resource active fedora model

---
+++ app/models/resource.rb
@@ -0,0 +1,17 @@
+class Resource < ActiveFedora::Base
+  def to_solr(solr_doc={}, options={})
+    solr_doc = super
+    content = downloadable_content
+    if content
+      solr_doc['downloadable_content_type_ssi'] = content.mimeType
+      solr_doc['downloadable_content_dsid_ssi'] = content.dsid
+      solr_doc['downloadable_content_label_ss'] = content.label.blank? ? this.label : content.label
+    end
+    solr_doc
+  end
+  def downloadable_content
+    return datastreams['CONTENT'] if (datastreams.has_key?('CONTENT'))
+    return datastreams['content'] if (datastreams.has_key?('content'))
+    return nil
+  end
+end
