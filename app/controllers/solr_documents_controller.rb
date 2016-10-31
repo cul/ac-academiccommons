cab15e0232532f6f249ea672defd98ac4bcd2a4c
Benjamin Armintor <armintor@gmail.com>
2016-10-31 17:35:51 -0400

    add SolrDocumentsController to support Hyacinth publish, fixes ACHYDRA-24

---
+++ app/controllers/solr_documents_controller.rb
@@ -0,0 +1,69 @@
+# -*- encoding : utf-8 -*-
+class SolrDocumentsController < ApplicationController
+  include Blacklight::Catalog
+
+  def rsolr
+    @rsolr ||= begin
+      url = Rails.application.config.solr['url']
+      RSolr.connect(:url => url)
+    end
+  end
+
+  def authenticate
+    status = :unauthorized
+    authenticate_with_http_basic do |user, pass|
+      (Rails.application.secrets.index_api_creds || {}).tap do |config|
+        status = (config['name'] == user && config['password'] == pass) ? :ok : :forbidden
+      end
+    end
+    status
+  end
+
+  def update
+    logger.debug "reindexing #{params[:id]}"
+    unless (status = authenticate) == :ok
+      render status: status, plain: ''
+      return
+    end
+    # if no id, return bad request (400)
+    unless params[:id]
+      render plain: "missing id parameter", status: :bad_request
+      return
+    end
+    begin
+      obj = ActiveFedora::Base.find(params[:id])
+      obj.update_index
+      render status: :ok, plain: ''
+    rescue ActiveFedora::ObjectNotFoundError => e
+      render status: :not_found, plain: ''
+    end
+  end
+
+  def destroy
+    logger.debug "removing #{params[:id]} from index"
+    unless (status = authenticate) == :ok
+      render status: status, plain: ''
+      return
+    end
+    begin
+      rsolr.delete_by_id(params[:id])
+      rsolr.commit
+    rescue Exception => e
+      logger.warn e.message
+    end
+    render status: :ok, plain: ''
+  end
+
+  def show
+    unless (status = authenticate) == :ok
+      render status: status, plain: ''
+      return
+    end
+    doc = rsolr.find(filters: {id: "\"#{params[:id]}\""})["response"]["docs"].first
+    if doc
+      render json: doc
+    else
+      render status: :not_found, plain: ''
+    end
+  end
+end
