250739a88cc73e92fdf72577274047a9f50a2220
Carla Galarza <cmg2228@columbia.edu>
2016-11-01 14:47:51 -0400

    Getting user information from ldap

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
  include Blacklight::User
  include Cul::Omniauth::Users
 
-  #before_create :set_personal_info_via_ldap
-  #after_initialize :set_personal_info_via_ldap
+  before_create :set_personal_info_via_ldap
+  after_initialize :set_personal_info_via_ldap
 
   # acts_as_authentic do |c|
   #   c.validate_password_field = false
@@ -27,36 +27,31 @@ class User < ActiveRecord::Base
   end
 
 
-  # def set_personal_info_via_ldap
-  #
-  #   logger.info "============== /// ======="
-  #
-  #   if wind_login
-  #     entry = Net::LDAP.new({:host => "ldap.columbia.edu", :port => 389}).search(:base => "o=Columbia University, c=US", :filter => Net::LDAP::Filter.eq("uid", wind_login)) || []
-  #     entry = entry.first
-  #
-  #     if entry
-  #       # self.email = entry[:mail].class.to_s
-  #       # self.last_name = entry[:sn].to_s
-  #       # self.first_name = entry[:givenname].to_s
-  #
-  #       entry[:mail].kind_of?(Array) ? self.email = entry[:mail].first.to_s : self.email = entry[:mail].to_s
-  #       entry[:sn].kind_of?(Array) ? self.last_name = entry[:sn].first.to_s : self.last_name = entry[:sn].to_s
-  #       entry[:givenname].kind_of?(Array) ? self.first_name = entry[:givenname].first.to_s : self.first_name = entry[:givenname].to_s
-  #
-  #
-  #
-  #       logger.info "login: " + login
-  #       logger.info "email: " + email
-  #       logger.info "last_name: " + last_name
-  #       logger.info "first_name: " + first_name
-  #       logger.info "============== end ======="
-  #
-  #
-  #     end
-  #   end
-  #
-  #   return self
-  # end
+  def set_personal_info_via_ldap
+    logger.info "============== /// ======="
+
+    if uid
+      entry = Net::LDAP.new({:host => "ldap.columbia.edu", :port => 389}).search(:base => "o=Columbia University, c=US", :filter => Net::LDAP::Filter.eq("uid", uid)) || []
+      entry = entry.first
+
+      if entry
+        # self.email = entry[:mail].class.to_s
+        # self.last_name = entry[:sn].to_s
+        # self.first_name = entry[:givenname].to_s
+
+        entry[:mail].kind_of?(Array) ? self.email = entry[:mail].first.to_s : self.email = entry[:mail].to_s
+        entry[:sn].kind_of?(Array) ? self.last_name = entry[:sn].first.to_s : self.last_name = entry[:sn].to_s
+        entry[:givenname].kind_of?(Array) ? self.first_name = entry[:givenname].first.to_s : self.first_name = entry[:givenname].to_s
+
+        logger.info "uid: " + uid
+        logger.info "email: " + email
+        logger.info "last_name: " + last_name
+        logger.info "first_name: " + first_name
+        logger.info "============== end ======="
+      end
+    end
+
+    return self
+  end
 
 end
