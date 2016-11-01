f6203a08bf35b7c976068eece0b3d29c767ecfdc
Carla Galarza <cmg2228@columbia.edu>
2016-11-01 13:46:35 -0400

    Removing old authentication helpers.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ActiveRecord::Base
   #before_create :set_personal_info_via_ldap
   #after_initialize :set_personal_info_via_ldap
 
-  acts_as_authentic do |c|
-    c.validate_password_field = false
-  end
+  # acts_as_authentic do |c|
+  #   c.validate_password_field = false
+  # end
 
   def admins
     where(:admin => true)
@@ -22,36 +22,36 @@ class User < ActiveRecord::Base
     end
   end
 
-  def set_personal_info_via_ldap
-
-    logger.info "============== /// ======="
-
-    if wind_login
-      entry = Net::LDAP.new({:host => "ldap.columbia.edu", :port => 389}).search(:base => "o=Columbia University, c=US", :filter => Net::LDAP::Filter.eq("uid", wind_login)) || []
-      entry = entry.first
-
-      if entry
-        # self.email = entry[:mail].class.to_s
-        # self.last_name = entry[:sn].to_s
-        # self.first_name = entry[:givenname].to_s
-
-        entry[:mail].kind_of?(Array) ? self.email = entry[:mail].first.to_s : self.email = entry[:mail].to_s
-        entry[:sn].kind_of?(Array) ? self.last_name = entry[:sn].first.to_s : self.last_name = entry[:sn].to_s
-        entry[:givenname].kind_of?(Array) ? self.first_name = entry[:givenname].first.to_s : self.first_name = entry[:givenname].to_s
-
-
-
-        logger.info "login: " + login
-        logger.info "email: " + email
-        logger.info "last_name: " + last_name
-        logger.info "first_name: " + first_name
-        logger.info "============== end ======="
-
-
-      end
-    end
-
-    return self
-  end
+  # def set_personal_info_via_ldap
+  #
+  #   logger.info "============== /// ======="
+  #
+  #   if wind_login
+  #     entry = Net::LDAP.new({:host => "ldap.columbia.edu", :port => 389}).search(:base => "o=Columbia University, c=US", :filter => Net::LDAP::Filter.eq("uid", wind_login)) || []
+  #     entry = entry.first
+  #
+  #     if entry
+  #       # self.email = entry[:mail].class.to_s
+  #       # self.last_name = entry[:sn].to_s
+  #       # self.first_name = entry[:givenname].to_s
+  #
+  #       entry[:mail].kind_of?(Array) ? self.email = entry[:mail].first.to_s : self.email = entry[:mail].to_s
+  #       entry[:sn].kind_of?(Array) ? self.last_name = entry[:sn].first.to_s : self.last_name = entry[:sn].to_s
+  #       entry[:givenname].kind_of?(Array) ? self.first_name = entry[:givenname].first.to_s : self.first_name = entry[:givenname].to_s
+  #
+  #
+  #
+  #       logger.info "login: " + login
+  #       logger.info "email: " + email
+  #       logger.info "last_name: " + last_name
+  #       logger.info "first_name: " + first_name
+  #       logger.info "============== end ======="
+  #
+  #
+  #     end
+  #   end
+  #
+  #   return self
+  # end
 
 end
