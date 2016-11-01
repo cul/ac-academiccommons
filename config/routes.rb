b0db77bb6b7f560413b8c9dd37588a67b0a2f0ca
Carla Galarza <cmg2228@columbia.edu>
2016-11-01 15:20:14 -0400

    Added logout link.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 AcademicCommons::Application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  devise_scope :user do
+    get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
+  end
+
   root :to => "catalog#index"
 
   get "info/about"
