ceb59617a090cece3cc56494f1a0a51aac9bfd91
Carla Galarza <cmg2228@columbia.edu>
2016-11-02 10:47:32 -0400

    Better logging and cleanup.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,6 @@ class User < ActiveRecord::Base
   before_create :set_personal_info_via_ldap
   after_initialize :set_personal_info_via_ldap
 
-  # acts_as_authentic do |c|
-  #   c.validate_password_field = false
-  # end
-
   def admins
     where(:admin => true)
   end
@@ -18,6 +14,7 @@ class User < ActiveRecord::Base
     full_name # TODO: or uni?
   end
 
+  # Password methods required by Devise.
   def password
     Devise.friendly_token[0,20]
   end
@@ -27,30 +24,19 @@ class User < ActiveRecord::Base
   end
 
   def set_personal_info_via_ldap
-    logger.info "============== /// ======="
-
     if uid
       entry = Net::LDAP.new({:host => "ldap.columbia.edu", :port => 389}).search(:base => "o=Columbia University, c=US", :filter => Net::LDAP::Filter.eq("uid", uid)) || []
       entry = entry.first
 
       if entry
-        # self.email = entry[:mail].class.to_s
-        # self.last_name = entry[:sn].to_s
-        # self.first_name = entry[:givenname].to_s
-
         entry[:mail].kind_of?(Array) ? self.email = entry[:mail].first.to_s : self.email = entry[:mail].to_s
         entry[:sn].kind_of?(Array) ? self.last_name = entry[:sn].first.to_s : self.last_name = entry[:sn].to_s
         entry[:givenname].kind_of?(Array) ? self.first_name = entry[:givenname].first.to_s : self.first_name = entry[:givenname].to_s
 
-        logger.info "uid: " + uid
-        logger.info "email: " + email
-        logger.info "last_name: " + last_name
-        logger.info "first_name: " + first_name
-        logger.info "============== end ======="
+        logger.info "Retriving/updating user information via LDAP for #{first_name} #{last_name} (#{uid})"
       end
     end
 
     return self
   end
-
 end
