f270e83a560eef0234911920365d3890b5717b81
Carla Galarza <cmg2228@columbia.edu>
2016-11-04 09:29:05 -0400

    Removing emails.yml from capistrano list of linked files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,6 @@ set :linked_files, fetch(:linked_files, []).push(
   "config/solr.yml",
   "config/fedora.yml",
   "config/indexing.yml",
-  "config/emails.yml",
   "config/google_analytics.yml",
   "config/secrets.yml",
   "public/robots.txt",
