f163bcd41b5a0bb7d18981870cb74c4fe9e5c02e
Carla Galarza <cmg2228@columbia.edu>
2017-02-10 16:31:31 -0500

    Removing log message. Using explicit call to url helpers because this class is used in libraries outside the context of url_helpers.

--- lib/academic_commons/listable.rb
+++ lib/academic_commons/listable.rb
@@ -6,17 +6,16 @@ module AcademicCommons
       uri_prefix = "info:fedora/"
 
       member_search = {
-      q: '*:*',
-      qt: 'standard',
-      fl: '*',
-      fq: ["cul_member_of_ssim:\"info:fedora/#{obj_display}\""],
-      rows: 10000,
-      facet: false
+        q: '*:*',
+        qt: 'standard',
+        fl: '*',
+        fq: ["cul_member_of_ssim:\"info:fedora/#{obj_display}\""],
+        rows: 10000,
+        facet: false
       }
       member_search[:fq] << "object_state_ssi:A" unless include_inactive
       response = Blacklight.default_index.connection.get 'select', params: member_search
       docs = response['response']['docs']
-      logger.debug "standard qt got #{docs.length} resources"
       docs.map do |member|
         res = {}
         member = SolrDocument.new(member)
@@ -25,7 +24,7 @@ module AcademicCommons
         res[:pid] = member_pid
         res[:filename] = member['downloadable_content_label_ss']
         dsid = member['downloadable_content_dsid_ssi']
-        res[:download_path] = fedora_content_path(:download, member_pid, dsid, res[:filename])
+        res[:download_path] = Rails.application.routes.url_helpers.fedora_content_path(:download, member_pid, dsid, res[:filename])
         res[:content_type] = member['downloadable_content_type_ssi']
 
         res
