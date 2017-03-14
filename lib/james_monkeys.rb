480f27c1551e6dfdbd8ec1fe289e927c6a154cf9
Carla Galarza <cmg2228@columbia.edu>
2017-03-14 15:15:02 -0400

    Deleting methods that are no longer needed.

--- lib/james_monkeys.rb
+++ lib/james_monkeys.rb
@@ -5,23 +5,4 @@ class Object
     collection = (args.length == 1 ? args.first : args)
     collection ? collection.include?(self) : false
   end
-
-  def listify(opts = {})
-    case self
-    when NilClass
-      opts[:include_nil] ? [nil] : []
-    when Array
-      self
-    else
-      [self]
-    end
-
-  end
-end
-
-class Hash
-  # creates a hash of arbitrary depth: you can refer to nested hashes without initialization.
-  def self.arbitrary_depth
-    Hash.new(&(p=lambda{|h,k| h[k] = Hash.new(&p)}))
-  end
 end
