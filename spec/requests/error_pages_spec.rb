520eda4ceee642a4f0e38fd7b669353aeb0dbaff
Carla Galarza <cmg2228@columbia.edu>
2017-04-12 15:04:05 -0400

    Adding test for successful admin page render.

--- spec/requests/error_pages_spec.rb
+++ spec/requests/error_pages_spec.rb
@@ -2,18 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'error pages', type: :request do
   include Warden::Test::Helpers
-  
+
   describe '/admin' do
     let(:uid) { 'abc123' }
 
+    before :each do
+      OmniAuth.config.test_mode = true
+      Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
+      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:saml]
+      login_as user
+      get '/admin'
+    end
+
     context 'when user not admin' do
-      before :each do
-        OmniAuth.config.test_mode = true
-        Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
-        Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:saml]
-        login_as User.new(uid: uid)
-        get '/admin'
-      end
+      let(:user) { User.new(uid: uid) }
 
       it 'returns 403 status code' do
         expect(response).to have_http_status(:forbidden)
@@ -21,6 +23,18 @@ RSpec.describe 'error pages', type: :request do
 
       it 'render forbidden page' do
         expect(response).to render_template('errors/forbidden')
+      end
+    end
+
+    context 'when user admin' do
+      let(:user) { User.new(uid: uid, admin: true) }
+
+      it 'returns 200 status code' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'render admin page' do
+        expect(response).to render_template('admin/index')
       end
     end
   end
