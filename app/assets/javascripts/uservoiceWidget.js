d9e2bb38407ef72451b60a45afb8fb7832173b2b
Carla Galarza <cmg2228@columbia.edu>
2017-07-18 10:51:38 -0400

    Remove uservoice javascript.

--- app/assets/javascripts/uservoiceWidget.js
+++
@@ -1,8 +0,0 @@
-// This is used by the support popup, that comes up when the user clicks the fandq links.
-
-var uvOptions = {};
-  (function() {
-    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
-    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/awPGkS3r9FKU3BChz0idQ.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
-  })();
