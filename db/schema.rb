f3eb2a2329ce5e6e8817674b2ea5cd9ad263fae8
Carla Galarza <cmg2228@columbia.edu>
2017-08-21 12:29:39 -0400

    Changing identifier field in Notification table to be named doi.

--- db/schema.rb
+++ db/schema.rb
@@ -11,80 +11,80 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815201919) do
+ActiveRecord::Schema.define(version: 20170821153702) do
 
   create_table "agreements", force: :cascade do |t|
-    t.string   "uni",               limit: 255
-    t.string   "name",              limit: 255
-    t.string   "email",             limit: 255
-    t.string   "agreement_version", limit: 255
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.string   "uni"
+    t.string   "name"
+    t.string   "email"
+    t.string   "agreement_version"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "bookmarks", force: :cascade do |t|
-    t.integer  "user_id",                 null: false
-    t.string   "document_id", limit: 255
-    t.string   "title",       limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.string   "user_type",   limit: 255
+    t.integer  "user_id",     null: false
+    t.string   "document_id"
+    t.string   "title"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "user_type"
   end
 
   create_table "content_blocks", force: :cascade do |t|
-    t.string   "title",      limit: 255, null: false
-    t.integer  "user_id",                null: false
+    t.string   "title",      null: false
+    t.integer  "user_id",    null: false
     t.text     "data"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "content_blocks", ["title"], name: "index_content_blocks_on_title"
 
   create_table "deposits", force: :cascade do |t|
-    t.string   "agreement_version", limit: 255,                 null: false
-    t.string   "uni",               limit: 255
-    t.string   "name",              limit: 255,                 null: false
-    t.string   "email",             limit: 255,                 null: false
-    t.string   "file_path",         limit: 255,                 null: false
-    t.text     "title",                                         null: false
-    t.text     "authors",                                       null: false
-    t.text     "abstract",                                      null: false
-    t.string   "url",               limit: 255
-    t.string   "doi_pmcid",         limit: 255
+    t.string   "agreement_version",                 null: false
+    t.string   "uni"
+    t.string   "name",                              null: false
+    t.string   "email",                             null: false
+    t.string   "file_path",                         null: false
+    t.text     "title",                             null: false
+    t.text     "authors",                           null: false
+    t.text     "abstract",                          null: false
+    t.string   "url"
+    t.string   "doi_pmcid"
     t.text     "notes"
-    t.boolean  "archived",                      default: false
-    t.datetime "created_at",                                    null: false
-    t.datetime "updated_at",                                    null: false
+    t.boolean  "archived",          default: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
   end
 
   create_table "email_preferences", force: :cascade do |t|
-    t.string   "author",          limit: 255, null: false
+    t.string   "author",          null: false
     t.boolean  "monthly_opt_out"
-    t.string   "email",           limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_index "email_preferences", ["author", "monthly_opt_out"], name: "index_email_preferences_on_author_and_monthly_opt_out"
 
   create_table "eventlogs", force: :cascade do |t|
-    t.string   "event_name", limit: 255
-    t.string   "user_name",  limit: 255
-    t.string   "uid",        limit: 255
-    t.string   "ip",         limit: 255
-    t.string   "session_id", limit: 255
+    t.string   "event_name"
+    t.string   "user_name"
+    t.string   "uid"
+    t.string   "ip"
+    t.string   "session_id"
     t.datetime "timestamp"
   end
 
   create_table "logvalues", force: :cascade do |t|
     t.integer "eventlog_id"
-    t.string  "param_name",  limit: 255
-    t.string  "value",       limit: 255
+    t.string  "param_name"
+    t.string  "value"
   end
 
   create_table "notifications", force: :cascade do |t|
-    t.string   "identifier", null: false
+    t.string   "doi",        null: false
     t.string   "kind",       null: false
     t.string   "email"
     t.string   "uni"
@@ -94,37 +94,37 @@ ActiveRecord::Schema.define(version: 20170815201919) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "notifications", ["identifier"], name: "index_notifications_on_identifier"
+  add_index "notifications", ["doi"], name: "index_notifications_on_doi"
 
   create_table "searches", force: :cascade do |t|
     t.text     "query_params"
     t.integer  "user_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.string   "user_type",    limit: 255
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "user_type"
   end
 
   add_index "searches", ["user_id"], name: "index_searches_on_user_id"
 
   create_table "sessions", force: :cascade do |t|
-    t.string   "session_id", limit: 255, null: false
+    t.string   "session_id", null: false
     t.text     "data"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "sessions", ["session_id"], name: "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
 
   create_table "statistics", force: :cascade do |t|
-    t.string   "session_id", limit: 255
-    t.string   "event",      limit: 255, null: false
-    t.string   "ip_address", limit: 255
-    t.string   "identifier", limit: 255
-    t.string   "result",     limit: 255
-    t.datetime "at_time",                null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "session_id"
+    t.string   "event",      null: false
+    t.string   "ip_address"
+    t.string   "identifier"
+    t.string   "result"
+    t.datetime "at_time",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "statistics", ["at_time"], name: "index_statistics_on_at_time"
@@ -134,7 +134,7 @@ ActiveRecord::Schema.define(version: 20170815201919) do
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
-    t.string   "taggable_type", limit: 255
+    t.string   "taggable_type"
     t.datetime "created_at"
   end
 
@@ -142,26 +142,26 @@ ActiveRecord::Schema.define(version: 20170815201919) do
   add_index "taggings", ["taggable_id", "taggable_type"], name: "index_taggings_on_taggable_id_and_taggable_type"
 
   create_table "tags", force: :cascade do |t|
-    t.string "name", limit: 255
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name",             limit: 255
-    t.string   "last_name",              limit: 255
+    t.string   "first_name"
+    t.string   "last_name"
     t.boolean  "admin"
-    t.string   "uid",                    limit: 255,             null: false
-    t.string   "email",                  limit: 255
-    t.string   "crypted_password",       limit: 255
-    t.string   "persistence_token",      limit: 255
-    t.integer  "sign_in_count",                      default: 0, null: false
+    t.string   "uid",                                null: false
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "persistence_token"
+    t.integer  "sign_in_count",          default: 0, null: false
     t.text     "last_search_url"
     t.datetime "last_sign_in_at"
     t.datetime "last_request_at"
     t.datetime "current_sign_in_at"
-    t.string   "last_sign_in_ip",        limit: 255
-    t.string   "current_sign_in_ip",     limit: 255
-    t.datetime "created_at",                                     null: false
-    t.datetime "updated_at",                                     null: false
+    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip"
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
--- spec/factories/notifications.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :notification do
-    identifier "MyString"
-    type ""
-    sent_at "2017-08-11 17:20:42"
-    email "MyString"
-  end
-end
