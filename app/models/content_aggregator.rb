c21560fc958ad492eae6e337371b7515df6573e3
Carla Galarza <cmg2228@columbia.edu>
2017-08-22 14:23:50 -0400

    Removing old method used for reindexing purposes.

--- app/models/content_aggregator.rb
+++ app/models/content_aggregator.rb
@@ -2,23 +2,6 @@ class ContentAggregator < ActiveFedora::Base
   include AcademicCommons::Indexable
   include AcademicCommons::Aggregator
 
-  def index_for_ac2(options={})
-    solr_doc = nil
-    error_message = ''
-    begin
-      solr_doc = to_solr({},options)
-      status = solr_doc.blank? ? :invalid_format : :success
-    rescue Exception => e
-      status = :error
-      error_message += e.message
-      Rails.logger.info "=== indexing error: " + e.message
-      Rails.logger.debug e
-    end
-
-    result = { results: solr_doc, status: status, error_message: error_message }
-    result
-  end
-
   def descMetadata_datastream
     if datastreams.keys.include?('descMetadata')
       return datastreams['descMetadata']
