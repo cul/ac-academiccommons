951d9b7ef4b9dd1ae9965130d818e766819fb994
Carla Galarza <cmg2228@columbia.edu>
2018-02-09 16:21:24 -0500

    Remove previous stylesheets and javascript, move to using default blacklight design.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 //
-//= require jquery
-//= require fancybox
-//= require jquery-ui-1.8.1.custom.min
-//= require custom
-//= require jquery.ui.selectmenu
-//= require rails
+
+//= require blacklight/blacklight
+//= require jquery_ujs
