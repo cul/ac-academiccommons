428c8775bd2e1682b79159c136e8a7e464d46f19
Carla Galarza <cmg2228@columbia.edu>
2018-02-19 12:37:02 -0500

    Fixing sitemap controller tests.

--- spec/features/sitemap_controller_spec.rb
+++ spec/features/sitemap_controller_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
-describe SitemapController, :type => :feature do
-  include Capybara::DSL
+describe SitemapController, type: :feature do
   before do
     @lmd = Time.now.httpdate # rfc2822
     solr_doc = {'id' => 'example_id', 'record_creation_date' => @lmd}
     solr = double('Solr')
     solr_response = double('SolrResponse')
-    allow(Blacklight).to receive(:solr).and_return(solr)
+    allow(Blacklight).to receive(:default_index).and_return(solr)
     allow(solr_response).to receive(:docs).and_return([solr_doc])
     allow(solr).to receive(:find).and_return(solr_response)
   end
