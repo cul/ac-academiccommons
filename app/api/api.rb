ab5e5d08cc75c043e125e3a4c098418fbc2234cc
Carla Galarza <cmg2228@columbia.edu>
2018-02-28 17:03:25 -0500

    Adding CORS headers to api.

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,4 +1,10 @@
 class API < Grape::API
+  # Adding CORS headers.
+  before do
+    header['Access-Control-Allow-Origin'] = '*'
+    header['Access-Control-Request-Method'] = '*'
+  end
+
   version 'v1', using: :path
   prefix :api
 
