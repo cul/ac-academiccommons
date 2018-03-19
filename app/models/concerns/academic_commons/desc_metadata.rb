82ce2ad8a7267373911a9b344072ef5a43afae03
Carla Galarza <cmg2228@columbia.edu>
2018-03-19 12:45:42 -0400

    Removing pid field and replacing with fedora3_pid_ssi

--- app/models/concerns/academic_commons/desc_metadata.rb
+++ app/models/concerns/academic_commons/desc_metadata.rb
@@ -67,7 +67,7 @@ module AcademicCommons
       # baseline blacklight fields: id is the unique identifier, format determines by default, what partials get called
       # TODO: Make sure access is indifferent
       add_field.call('id', pid) unless solr_doc['id'] || solr_doc[:id]
-      add_field.call('pid', pid) unless solr_doc['pid'] || solr_doc[:pid]
+      add_field.call('fedora3_pid_ssi', pid) unless solr_doc['pid'] || solr_doc[:pid]
 
       Array.wrap(belongs_to).each do |collection|
         add_field.call('member_of', collection)
