863c1076e7439ea631fd9a0cd3f4012adc1effcb
Carla Galarza <cmg2228@columbia.edu>
2018-03-28 11:52:20 -0400

    Adding application and capistrano configuration for new environment

---
+++ config/environments/ac4_test.rb
@@ -0,0 +1,23 @@
+require Rails.root.join("config/environments/academiccommons_prod")
+
+AcademicCommons::Application.configure do
+  # Expands the lines which load the assets
+  config.assets.debug = true
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  # Setting host so that url helpers can be used in mailer views.
+  config.action_mailer.default_url_options = { host: 'ac4-test.cul.columbia.edu' }
+
+  # Use the lowest log level to ensure availability of diagnostic information
+  # when problems arise.
+  config.log_level = :debug
+
+  # Application specific configuration.
+  config.analytics_enabled = false
+  config.base_path = "ac4-test.cul.columbia.edu"
+  config.prod_environment = false
+end
