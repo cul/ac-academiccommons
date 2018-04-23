d41c3f6f3878692254dc21a48677781507c0f3f0
Ed Madrid <edmadrid@gmail.com>
2018-04-23 14:49:23 -0400

    fixes issue with view more authors appearing in facets nav

--- app/assets/javascripts/readmore-call.js
+++ app/assets/javascripts/readmore-call.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.blacklight-author_ssim').readmore({
+  $('#document .blacklight-author_ssim').readmore({
   collapsedHeight: 25,
   lessLink: '<a href="#">View fewer authors</a>',
   moreLink: '<a href="#">View all authors</a>'
