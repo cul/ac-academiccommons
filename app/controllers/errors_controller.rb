fb1113abc7134febe2e7b70d9271d3dbe5bb78f7
Carla Galarza <cmg2228@columbia.edu>
2018-04-23 14:36:50 -0400

    Fixing error raised when callback method is missing.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,7 @@ class ErrorsController < ApplicationController
   include Gaffe::Errors
 
   # Make sure anonymous users can see the page
-  skip_before_action :require_admin!
+  skip_before_action :require_admin!, raise: false
 
   def internal_server_error; end
 end
