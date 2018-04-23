e9acb2e1b8ae10b38d8c1f176003dd63a4df7ea6
Ed Madrid <edmadrid@gmail.com>
2018-04-23 15:08:20 -0400

    fixes styling issue with view all authors button

--- app/assets/javascripts/readmore-call.js
+++ app/assets/javascripts/readmore-call.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $('#document .blacklight-author_ssim').readmore({
   collapsedHeight: 25,
   lessLink: '<a href="#">View fewer authors</a>',
-  moreLink: '<a href="#">View all authors</a>'
+  moreLink: '<a href="#">View all authors</a>',
+  embedCSS: false
   });
 });
