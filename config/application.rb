5f3ef3b196e2d95155c665a6d73404ad2d19bba7
Carla Galarza <cmg2228@columbia.edu>
2018-04-24 10:49:43 -0400

    Ran app:update for Rails 5.2 updates.

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,10 @@ Bundler.require(*Rails.groups)
 module AcademicCommons
   class Application < Rails::Application
     include Cul::Omniauth::FileConfigurable
+
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
