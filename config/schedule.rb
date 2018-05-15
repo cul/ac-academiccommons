ca718cfeed234dd1666187123bb7e7b6cf6953fc
Carla Galarza <cmg2228@columbia.edu>
2018-05-15 10:08:59 -0400

    Adding sitemap generation to cron
    - Better names for cron emails
    - Generating sitemap every weekday at 6pm.

--- config/schedule.rb
+++ config/schedule.rb
@@ -11,14 +11,19 @@ set :environment, Rails.env
 # This is a CUL provided script. More details can be found here:
 # https://wiki.library.columbia.edu/display/USGSERVICES/Cron+Management
 # Errors will be emailed out to email specified in secrets.yml.
-set :email_subject, "Cron"
+set :email_subject, 'Cron'
 set :error_recipient, Rails.application.secrets[:cron_errors]
 set :job_template, "/usr/local/bin/mailifrc -s 'Error - :email_subject' :error_recipient -- /bin/bash -l -c ':job'"
 
 # Overriding to remove output redirection option.
-job_type :rake, "cd :path && :environment_variable=:environment bundle exec rake :task"
+job_type :rake, 'cd :path && :environment_variable=:environment bundle exec rake :task'
 
-# Delete searches table weekly.
+# Delete searches table daily.
 every :day, at: '5am' do
-  rake "blacklight:delete_old_searches[7]", email_subject: 'Weekly searches table cleanout'
+  rake 'blacklight:delete_old_searches[7]', email_subject: 'Searches table cleanout'
+end
+
+# Regenerate sitemap every weekday.
+every :weekday, at: '6pm' do
+  rake 'sitemap:create', email_subject: 'Sitemap generation'
 end
