f34805b804f92543c9b0b07a61f1ec3bf8125866
Carla Galarza <cmg2228@columbia.edu>
2018-05-31 14:06:06 -0400

    Creating routes and controller for /upload

---
+++ app/controllers/uploads_controller.rb
@@ -0,0 +1,12 @@
+class UploadsController < ApplicationController
+  # GET /upload
+  # Show deposit landing page page if user is not logged in. If user is logged
+  # in redirect to /upload/new
+  def index
+    redirect_to action: :new unless current_user.nil?
+  end
+
+  # GET /upload/new
+  # Show deposit form, if user is logged in.
+  def new; end
+end
