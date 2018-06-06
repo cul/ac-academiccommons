3306b22df1fd75aa74f1daba540dcb12ed5ae261
Carla Galarza <cmg2228@columbia.edu>
2018-06-06 16:24:38 -0400

    Switching /deposit to /upload

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -11,24 +11,25 @@ class UploadsController < ApplicationController
   # GET /upload/new
   # Show upload form, if user is logged in.
   def new
-    @deposit = Deposit.new
+    @deposit ||= Deposit.new
   end
 
   # POST /upload
   def create
-    deposit = Deposit.new(upload_params)
-    deposit.files.attach(params[:deposit][:files])
-    deposit.user = current_user
+    @deposit = Deposit.new(upload_params)
+    @deposit.files.attach(params[:deposit][:files])
+    @deposit.user = current_user
+    @deposit.authenticated = true
     # TODO: save user name and uni in deposit object
 
     # TODO: validate that required fields are inputted
     # required fields: 1 creator, right statement, title, file, abstract and year
-    if deposit.save
+    if @deposit.save
       render template: 'uploads/successful_upload'
     else
       flash[:error] = 'Error saving deposit, please make sure all required fields are entered.'
-      Rails.logger.debug deposit.errors.inspect
-      redirect_to action: :new
+      Rails.logger.debug @deposit.errors.inspect
+      render :new
     end
   end
 
