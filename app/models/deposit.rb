ee6be47acff3db01c958124e5224d2a4bee8dab7
Carla Galarza <cmg2228@columbia.edu>
2018-06-06 14:53:33 -0400

    Adding license and rights statement dropdown

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,5 +1,23 @@
 class Deposit < ApplicationRecord
+  RIGHTS_STATEMENTS = [
+    'No Copyright - United States',
+    'No Copyright - Other Known Legal Restrictions',
+    'In Copyright',
+    'In Copyright - Educational Use Permitted',
+    'In Copyright- Non-Commercial Use Permitted',
+    'In Copyright- Unknown Rightsholder',
+    'Copyright Undetermined'
+  ].freeze
 
+  LICENSE = [
+    'Attribution (CC BY)',
+    'Attribution-ShareAlike (CC BY-SA)',
+    'Attribution-NoDerivs (CC BY-ND)',
+    'Attribution-NonCommercial (CC BY-NC)',
+    'Attribution-NonCommercial-ShareAlike (CC BY-NC-SA)',
+    'Attribution-NonCommercial-NoDerivs (CC BY-NC-ND)',
+    'No license attributed'
+  ].freeze
   # validates_presence_of :agreement_version
   # validates_presence_of :name
   # validates_presence_of :email
