708eae9dd3fdf865ff52c66f56f0a7e84271d481
edmadrid <edmadrid@gmail.com>
2018-06-20 16:22:38 -0400

    modifies homepage test

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -12,7 +12,7 @@ describe 'Homepage', type: :feature do
   end
 
   it 'links to the about page' do
-    within('.about-links') do
+    within('.main-nav') do
       expect(page).to have_link('About', href: '/about')
       click_link 'About'
     end
