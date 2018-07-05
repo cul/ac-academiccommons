9e83d89e280cfb092420063f610e11d231219d56
Carla Galarza <cmg2228@columbia.edu>
2018-07-05 17:36:58 -0400

    Adding icons to file upload

--- app/assets/javascripts/upload/file-upload.js
+++ app/assets/javascripts/upload/file-upload.js
@@ -20,20 +20,21 @@ $(document).ready(function() {
     dictDefaultMessage: "<span class='link-color'>Select a file</span> or drag and drop here",
     url: directUploadUrl,
     paramName: fileVariableName,
-    // uploadMultiple: true, // disabling because we treat each file individually.
+    uploadMultiple: false, // disabling because we treat each file individually.
     hiddenInputContainer: 'div#file-upload',
     createImageThumbnails: false,
     previewTemplate: previewTemplate,
     init: function(){
       this._uploadData = function(files, dataBlocks){
         for (var i = 0; i < files.length; i++) {
-          var upload = new ActiveStorage.DirectUpload(files[i], this.options.url);
+          var file = files[i]
+          var upload = new ActiveStorage.DirectUpload(file, this.options.url);
           var dropzone = this;
 
           upload.create(function (error, blob){
             if (error) {
               // Handle the error
-              dropzone._errorProcessing([files[i]], 'Error Uploading File');
+              dropzone._errorProcessing([file], 'Error Uploading File');
             } else {
               //  Add an appropriately-named hidden input to the form with a
               //  value of blob.signed_id so that the blob ids will be
@@ -43,7 +44,7 @@ $(document).ready(function() {
               hiddenField.setAttribute("value", blob.signed_id);
               hiddenField.name = dropzone.options.paramName;
               document.querySelector('form#upload').appendChild(hiddenField);
-              dropzone._finished([files[i]]);
+              dropzone._finished([file]);
             }
           })
         }
@@ -55,6 +56,5 @@ $(document).ready(function() {
   // We can revist this behavior when we can display this previously attached
   // documents in the UI.
   var hiddenFileInputs = document.querySelectorAll('input[name="deposit[files][]"][type="hidden"]');
-  console.log(hiddenFileInputs);
   hiddenFileInputs.forEach( function(currentValue, currentIndex, listObj) { currentValue.remove(); } );
 });
