f4c4a37022f5d57dc95555e86ef23799e0550328
Carla Galarza <cmg2228@columbia.edu>
2018-07-11 17:08:01 -0400

    Updating admin email preferences pages
    - Styling to fit with new page layout and bootstrap
    - Removed unnecessary show link from all pages
    - Added delete button to edit page
    - Remove old email preferences controller
    - Updated form to use form_with
    - Better error handeling in error pages

--- app/controllers/admin/email_preferences_controller.rb
+++ app/controllers/admin/email_preferences_controller.rb
@@ -7,34 +7,36 @@ module Admin
     end
 
     def show
-      @email_preference = EmailPreference.find(params[:id])
+      @email_preference ||= EmailPreference.find(params[:id])
     end
 
     def new
-      @email_preference = EmailPreference.new
+      @email_preference ||= EmailPreference.new
     end
 
     def create
       @email_preference = EmailPreference.new(email_preference_params)
       if @email_preference.save
         flash[:success] = 'Successfully created email preference.'
-        redirect_to @email_preference
+        render :show
       else
-        render action: :new
+        flash[:error] = @email_preference.errors.full_messages.to_sentence
+        render :new
       end
     end
 
     def edit
-      @email_preference = EmailPreference.find(params[:id])
+      @email_preference ||= EmailPreference.find(params[:id])
     end
 
     def update
       @email_preference = EmailPreference.find(params[:id])
       if @email_preference.update_attributes(email_preference_params)
         flash[:success] = 'Successfully updated email preference.'
-        redirect_to @email_preference
+        render :show
       else
-        render action: :edit
+        flash[:error] = @email_preference.errors.full_messages.to_sentence
+        render :edit
       end
     end
 
@@ -42,7 +44,7 @@ module Admin
       @email_preference = EmailPreference.find(params[:id])
       @email_preference.destroy
       flash[:success] = 'Successfully destroyed email preference.'
-      redirect_to email_preferences_url
+      redirect_to admin_email_preferences_url
     end
 
     private
