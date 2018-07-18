49e93f252fe97d0fb77831fae8a96246f5f0314a
Carla Galarza <cmg2228@columbia.edu>
2018-07-18 10:45:52 -0400

    Adding embargoed works section. Optionally displaying embargoed works and pending works section if there are works to display. Available works is always displays, thought it will have a message prompting users to deposit works if they have none.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,19 +6,28 @@ class UserController < ApplicationController
   def account; end
 
   def my_works
-    @pending_documents = pending_documents
-    @current_documents_with_stats = current_documents_with_stats
+    @pending_works = pending_works
+    @embargoed_works = embargoed_works
+    @current_works_with_stats = current_works_with_stats
   end
 
   private
 
-  def pending_documents
+  def pending_works
     current_user.deposits.to_a.keep_if do |deposit|
       deposit.hyacinth_identifier.blank?
     end
   end
 
-  def current_documents_with_stats
+  def embargoed_works
+    AcademicCommons.search do |params|
+      params.filter('author_uni_ssim', current_user.uid)
+      params.embargoed_only
+      params.aggregators_only
+    end
+  end
+
+  def current_works_with_stats
     startdate = Date.current.prev_month.beginning_of_month
     enddate   = startdate.end_of_month
     solr_params = { q: nil, fq: ["author_uni_ssim:\"#{current_user.uid}\""] }
