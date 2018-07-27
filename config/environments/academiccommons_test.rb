ea28893456fd278a17d9d45162fce4893315f097
Carla Galarza <cmg2228@columbia.edu>
2018-07-27 15:39:37 -0400

    Compressing assets on deploy to academiccommons_test

--- config/environments/academiccommons_test.rb
+++ config/environments/academiccommons_test.rb
@@ -1,8 +1,14 @@
 require Rails.root.join("config/environments/academiccommons_prod")
 
-Rails.application.configure do
+AcademicCommons::Application.configure do
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
+
+  # Compresses assets.
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
