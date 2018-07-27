57b9957eb148ddb1eab0850ce0fce883c9166a19
Carla Galarza <cmg2228@columbia.edu>
2018-07-27 15:25:48 -0400

    Using autoCommit when indexing items

--- app/jobs/indexing_job.rb
+++ app/jobs/indexing_job.rb
@@ -1,8 +1,12 @@
 class IndexingJob < ApplicationJob
   # Indexes the pid provided
   def perform(pid)
-    i = ActiveFedora::Base.find(pid)
-    i.update_index
+    # Using direct solr query to index an item/asset without soft commiting.
+    # autoCommit will take care of presisting the new document. This change
+    # is required when indexing multiple items in parallel.
+    obj = ActiveFedora::Base.find(pid)
+    solr_doc = obj.to_solr
+    ActiveFedora::SolrService.add(solr_doc)
 
     Rails.cache.delete('repository_statistics') # Invalidating stats on homepage.
   end
