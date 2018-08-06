4c57f31e4f85e1df8651f51ed58a1bd116f3972c
Carla Galarza <cmg2228@columbia.edu>
2018-08-06 12:07:18 -0400

    Removing previous values inputted into text fields and dropdowns

--- app/assets/javascripts/duplicate-input-fields.js
+++ app/assets/javascripts/duplicate-input-fields.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
   $("[data-duplicate-fields-template]").click(function(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    fieldsTemplateClass = $(this).data("duplicate-fields-template")
-    clonedTemplate = $( "." + fieldsTemplateClass).last().clone()
-    clonedTemplate.insertAfter( $("." + fieldsTemplateClass).last() );
+    fieldsTemplateClass = $(this).data("duplicate-fields-template");
+    $( "." + fieldsTemplateClass).last()
+                                 .clone()
+                                 .find("input:text").val("").end()
+                                 .find("option:selected").removeAttr("selected").end()
+                                 .insertAfter( $("." + fieldsTemplateClass).last() );
   });
 })
