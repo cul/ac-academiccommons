8491087aa75df196e2526a7a11a3e660cd18e65e
Carla Galarza <cmg2228@columbia.edu>
2018-08-24 14:16:36 -0400

    Removing unused code

--- lib/academic_commons/metrics/output_csv.rb
+++ lib/academic_commons/metrics/output_csv.rb
@@ -4,39 +4,12 @@ module AcademicCommons::Metrics
   module OutputCSV
     MONTH_KEY = '%b %Y'.freeze
 
-    # CSV with monthly breakdown of stats for each event.
-    #
-    # @param [User] requested_by user the report was requested by
-    def to_csv_by_month(requested_by: nil)
-      # Can only be generated with the per month flag is on
-
-      CSV.generate do |csv|
-        # csv.add_row [facet.include?('author_ssim', 'author_uni_ssim') ? 'Author UNI/Name:' : 'Search criteria:', self.query]
-        csv.add_row [self.solr_params.inspect]
-        csv.add_row []
-        csv.add_row ['Period Covered by Report', time_period]
-        csv.add_row []
-        csv.add_row ['Report created by:', requested_by.nil? ? 'N/A' : "#{requested_by} (#{requested_by.uid})"]
-        csv.add_row ['Report created on:', Time.new.strftime('%Y-%m-%d') ]
-
-        add_usage_category(csv, 'Views', Statistic::VIEW)
-        add_usage_category(csv, 'Streams', Statistic::STREAM) if (options[:include_streaming])
-        add_usage_category(csv, 'Downloads', Statistic::DOWNLOAD)
-      end
-    end
-
     # Creates CSV with usage stats for the time period given. If a start and end
     # date were not specified Lifetime stats are displayed.
     def to_csv
       CSV.generate do |csv|
         csv.add_row [ 'Time Period:', time_period]
         csv.add_row []
-
-        # csv.add_row [ 'FACET', 'ITEM']
-        # params[:f].each do |key, value|
-        #   csv.add_row [facet_names[key.to_s], value.first]
-        # end
-        # csv.add_row []
 
         csv.add_row ['Query:', solr_params.inspect] # TODO: Pretty print for solr params.
         time = lifetime_only? ? 'Lifetime' : 'Period'
@@ -64,19 +37,6 @@ module AcademicCommons::Metrics
         end
       end
     end
-
-    # display: month_to_month
-    #          lifetime_summary
-    #          period_summary
-    # def to_csv(type)
-    #
-    # Headers (should be the same for both)
-    #
-    # if lifetime or range
-    #   fetch table
-    # elsif month to month, can only do month to month if the month to month flag was choosen
-    #   fetch different table (x2)
-    # end
 
     # Returns array with details of usage stats
     def report_details(period_covered: nil)
@@ -157,28 +117,6 @@ module AcademicCommons::Metrics
       end
 
       table
-    end
-
-    private
-
-    # Makes each category (View, Download, Streaming) section of csv.
-    def add_usage_category(csv, category, key)
-      csv.add_row []
-      csv.add_row []
-      csv.add_row [ "#{category} report:".upcase ]
-      csv.add_row [ 'Total for period:', total_for(key, 'Period').to_s,  '', '', '', "#{category} by Month"]
-      month_column_headers = self.months_list.map { |m| m.strftime('%b-%Y') }
-      csv.add_row [ 'Title', 'Content Type', 'Persistent URL', 'Publisher DOI', "Reporting Period Total #{category}"].concat(month_column_headers)
-
-      self.each do |item_stat|
-        id = item_stat.id
-        monthly_stats = self.months_list.map { |m| item_stat.get_stat(key, m.strftime(MONTH_KEY)) }
-        document = item_stat.document
-        csv.add_row [
-          document['title_ssi'], document['genre_ssim'].first, document['cul_doi_ssi'],
-          document['publisher_doi_ssi'], item_stat.get_stat(key, 'Period')
-        ].concat(monthly_stats)
-      end
     end
   end
 end
