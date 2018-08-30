806ed940239f04cad9ca837da02740882b0d793e
Carla Galarza <cmg2228@columbia.edu>
2018-08-30 14:21:53 -0400

    Adding test to check that embargoed items are displayed in the myworks page

--- spec/features/myworks_spec.rb
+++ spec/features/myworks_spec.rb
@@ -3,29 +3,65 @@ require 'rails_helper'
 describe 'myworks', type: :feature do
   include_context 'non-admin user for feature'
 
-  before do
-    FactoryBot.create(:view_stat)
-    FactoryBot.create(:download_stat)
-    FactoryBot.create(:download_stat)
-    FactoryBot.create(:view_stat, at_time: Time.current - 1.month)
-    visit myworks_path
-  end
+  context 'when author has available works' do
+    before do
+      FactoryBot.create(:view_stat)
+      FactoryBot.create(:download_stat)
+      FactoryBot.create(:download_stat)
+      FactoryBot.create(:view_stat, at_time: Time.current - 1.month)
+      visit myworks_path
+    end
 
-  it 'displays available works' do
-    expect(page).to have_content 'Alice\'s Adventures in Wonderland'
-  end
+    it 'displays available works' do
+      expect(page).to have_content 'Alice\'s Adventures in Wonderland'
+    end
 
-  it 'displays correct download statistics' do
-    within :xpath, '//tr/td/a[@href="/doi/10.7916/ALICE"]/../..' do
-      expect(page).to have_xpath 'td[2]', text: '0'
-      expect(page).to have_xpath 'td[3]', text: '2'
+    it 'displays correct download statistics' do
+      within :xpath, '//tr/td/a[@href="/doi/10.7916/ALICE"]/../..' do
+        expect(page).to have_xpath 'td[2]', text: '0'
+        expect(page).to have_xpath 'td[3]', text: '2'
+      end
+    end
+
+    it 'displays correct view statistics' do
+      within :xpath, '//tr/td/a[@href="/doi/10.7916/ALICE"]/../..' do
+        expect(page).to have_xpath 'td[4]', text: '1'
+        expect(page).to have_xpath 'td[5]', text: '2'
+      end
     end
   end
 
-  it 'displays correct view statistics' do
-    within :xpath, '//tr/td/a[@href="/doi/10.7916/ALICE"]/../..' do
-      expect(page).to have_xpath 'td[4]', text: '1'
-      expect(page).to have_xpath 'td[5]', text: '2'
+  context 'when author has embargoed works' do
+    let(:embargoed_solr_params) do
+      {
+        qt: 'search',
+        fq: ['author_uni_ssim:"tu123"', 'object_state_ssi:"A"', 'free_to_read_start_date_dtsi:[NOW+1DAYS TO *]', "has_model_ssim:\"#{ContentAggregator.to_class_uri}\""],
+        rows: 100_000
+      }
+    end
+    let(:solr_response) do
+      Blacklight::Solr::Response.new(
+        {
+          'response' => {
+            'docs' => [
+              { 'id' => '10.7916/TESTDOC10', 'title_ssi' => 'First Test Document', 'object_state_ssi' => 'A',
+                'cul_doi_ssi' => '10.7916/TESTDOC10', 'fedora3_pid_ssi' => 'actest:10', 'genre_ssim' => '', 'publisher_doi_ssi' => '',
+                'free_to_read_start_date_ssi' => Date.current.tomorrow.strftime('%Y-%m-%d') }
+            ]
+          }
+        }, {}
+      )
+    end
+
+    before do
+      allow(Blacklight.default_index).to receive(:search).with(any_args).and_call_original.once
+      allow(Blacklight.default_index).to receive(:search).with(embargoed_solr_params).and_return(solr_response)
+      visit myworks_path
+    end
+
+    it 'displays embargoed works' do
+      expect(page).to have_text 'Embargoed Works'
+      expect(page).to have_link 'First Test Document', href: '/doi/10.7916/TESTDOC10'
     end
   end
 end
