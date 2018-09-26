ff220910eafb5cd17a40a5a1a1a72904d036b6ed
Carla Galarza <cmg2228@columbia.edu>
2018-09-26 15:31:20 -0400

    Adding display name to from field in statistics email

--- app/mailers/statistics_mailer.rb
+++ app/mailers/statistics_mailer.rb
@@ -1,18 +1,16 @@
 class StatisticsMailer < ApplicationMailer
-  default from: Rails.application.config_for(:emails)['mail_deliverer']
+  default from: "Academic Commons <#{Rails.application.config_for(:emails)['mail_deliverer']}>"
 
   def author_monthly(to_address, author_id, usage_stats, optional_note)
     @author_id = author_id
     @usage_stats = usage_stats
     recipients = to_address
-    from = Rails.application.config_for(:emails)['mail_deliverer']
-    full_from = "\"Academic Commons\" <#{from}>"
 
     subject = "Academic Commons Monthly Download Report for #{@usage_stats.time_period}"
     @streams = @usage_stats.include_streaming
     @optional_note = optional_note
 
-    mail(to: recipients, from: full_from, subject: subject)
+    mail(to: recipients, subject: subject)
 
     logger.debug("Report sent for: #{author_id} to: #{to_address}")
   end
