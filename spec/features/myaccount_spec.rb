ce33edd4d9703ad638e8de30593d9f71fe405416
Carla Galarza <cmg2228@columbia.edu>
2018-10-12 16:09:10 -0400

    Adding feature test for my accounts page

--- spec/features/myaccount_spec.rb
+++ spec/features/myaccount_spec.rb
@@ -14,4 +14,25 @@ describe 'My Account', type: :feature do
   it 'renders read and sign agreement link' do
     expect(page).to have_css('a', text: 'Read and sign the agreement')
   end
+
+  it 'renders email preferences' do
+    expect(page).to have_content('Email Preferences')
+    expect(page).to have_content('You can change your Academic Commons email preferences below.')
+  end
+
+  context 'changing email preferences' do
+    let(:email_preference) { EmailPreference.first }
+
+    before do
+      fill_in 'Preferred Email', with: 'tu123@example.com'
+      check 'Unsubscribe all email communications'
+      click_button 'Save'
+    end
+
+    it 'creates correct user preferences' do
+      expect(email_preference.uni).to eql 'tu123'
+      expect(email_preference.email).to eql 'tu123@example.com'
+      expect(email_preference.unsubscribe).to be true
+    end
+  end
 end
