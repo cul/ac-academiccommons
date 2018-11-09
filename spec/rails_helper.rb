bedd44b718cde2e69234f7939514b17f802e55c2
Carla Galarza <cmg2228@columbia.edu>
2018-11-09 11:54:27 -0500

    Adding additional formatter in order to locally display coverage report.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,15 @@
-# Require coveralls before anything else.
+# Require simplecov/coveralls before anything else.
+require 'simplecov'
 require 'coveralls'
 Coveralls.wear!('rails')
+
+# Use both the coveralls formatter (for sending results to coveralls) and the
+# simplecov html output to have a local display of coverage.
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
+)
+
+SimpleCov.start
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
