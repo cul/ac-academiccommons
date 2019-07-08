905154627676f84cafc6a8df1044c987b1f22209
Benjamin Armintor <armintor@gmail.com>
2019-07-08 17:00:03 -0400

    add CIESIN as a Featured Partner (ACHYDRA-666)

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,10 +2,11 @@ class CollectionsController < ApplicationController
   CONFIG = {
     featured: {
       title: 'Featured Partners',
-      summary: 'Works shared by six Columbia research centers. Want to showcase the work of your department or center here? Contact us at <a href="mailto:ac@columbia.edu">ac@columbia.edu</a>',
+      summary: 'Works shared by seven Columbia research centers. Want to showcase the work of your department or center here? Contact us at <a href="mailto:ac@columbia.edu">ac@columbia.edu</a>',
       facet: 'department_ssim',
       values: {
         'Center for Behavioral Cardiovascular Health' => 'The Center for Behavioral Cardiovascular Health (CBCH) is a leader in cutting-edge behavioral medicine research dedicated to understanding how and why behaviors, psychological factors, and societal forces influence hypertension and cardiovascular disease. ',
+        'Center for International Earth Science Information Network' => 'The Center for International Earth Science Information Network (CIESIN) works at the intersection of the social, natural, and information sciences, and specializes in on-line data and information management, spatial data integration and training, and interdisciplinary research related to human interactions in the environment.',
         'Center on Japanese Economy and Business'     => 'The Center on Japanese Economy and Business (CJEB) is the preeminent US academic center focused on promoting knowledge of Japanese business systems in domestic, East Asia, and international contexts.',
         'Columbia Center on Sustainable Investment'   => 'The Columbia Center on Sustainable Investment (CCSI) is the only university-based applied research center and forum dedicated to the study, practice and discussion of sustainable international investment.',
         'Community College Research Center'           => 'CCRC strategically assesses the problems and performance of community colleges in order to contribute to the development of practice and policy that expands access to higher education and promotes success for all students.',
