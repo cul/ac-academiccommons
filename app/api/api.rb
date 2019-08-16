21081dc1c07ebf2e0bfc1b7f0e328a5a4e54a02b
Carla Galarza <cmg2228@columbia.edu>
2019-08-16 12:09:03 -0400

    Adding documentation about resource_paths

--- app/api/api.rb
+++ app/api/api.rb
@@ -25,13 +25,16 @@ class API < Grape::API
                    'records. `/search` results are limited to 100 results per page '\
                    'and only show the most commonly used fields. The `/record` '\
                    'endpoint will display all the fields. Frequent consumers '\
-                   'of this api, may be interested in setting up a `data_feed/`'\
-                   ' for their specific purposes. Data feeds require authentication '\
+                   'of this api may be interested in setting up a `data_feed/` endpoint'\
+                   'for their specific purposes. Data feeds require authentication '\
                    '(via HTTP Token Authentication), but have the benefit of '\
                    'displaying the entire record and not having an upper limit '\
                    'on the number of results displayed.'\
                    "\n\n All endpoints accept `format` as a query parameter to "\
-                   'specify format response instead of accept headers. For example: `format=json`.',
+                   'specify format response instead of accept headers. For example: `format=json`.'\
+                   "\n\n Records return a list of `resource_paths`. `resource_paths` are partial "\
+                   'paths that can be used to create a file download link. Preprend the base url '\
+                   'listed above to the paths in order to generate download links.',
       contact_name: 'Academic Commons Staff',
       contact_email: 'ac@columbia.edu'
     },
