6a52f776ec95d44e32af12258cea574b7adba0c0
Carla Galarza <cmg2228@columbia.edu>
2019-08-29 19:57:28 -0400

    Trying styling again

--- app/helpers/asset_helper.rb
+++ app/helpers/asset_helper.rb
@@ -9,17 +9,17 @@ module AssetHelper
     end
   end
 
-  def video_player(url, poster_path, width = 1024, height = 576)
+  def video_player(url, poster_path)
     tag.div class: 'mediaelement-player' do
-      tag.video class: 'mejs__player', width: width, height: height, style: 'width:100%;height:100%;', poster: poster_path, controls: 'controls', preload: 'none' do
+      tag.video class: 'mejs__player', style: 'width:100%;height:100%;', poster: poster_path, controls: 'controls', preload: 'none' do
         tag.source type: 'application/x-mpegURL', src: url
       end
     end
   end
 
-  def audio_player(url, width = 1024)
+  def audio_player(url)
     tag.div class: 'mediaelement-player' do
-      tag.audio class: 'mejs__player', width: width, style: 'width:100%;', controls: 'controls', preload: 'none' do
+      tag.audio class: 'mejs__player', style: 'width:100%;', controls: 'controls', preload: 'none' do
         tag.source type: 'application/x-mpegURL', src: url
       end
     end
