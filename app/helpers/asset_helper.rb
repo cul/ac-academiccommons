f762a68ac82e017fff0352369e376b8c7b10056b
Carla Galarza <cmg2228@columbia.edu>
2019-08-29 19:29:38 -0400

    Improving styling

--- app/helpers/asset_helper.rb
+++ app/helpers/asset_helper.rb
@@ -10,21 +10,17 @@ module AssetHelper
   end
 
   def video_player(url, poster_path, width = 1024, height = 576)
-    tag.div class: 'mejs-ted' do
-      tag.div class: 'mediaelement-player' do
-        tag.video class: 'mejs__player', width: width, height: height, style: 'width:100%;height:100%;', poster: poster_path, controls: 'controls', preload: 'none' do
-          tag.source type: 'application/x-mpegURL', src: url
-        end
+    tag.div class: 'mediaelement-player' do
+      tag.video class: 'mejs__player', width: width, height: height, style: 'width:100%;height:100%;', poster: poster_path, controls: 'controls', preload: 'none' do
+        tag.source type: 'application/x-mpegURL', src: url
       end
     end
   end
 
   def audio_player(url, width = 1024)
-    tag.div class: 'mejs-ted' do
-      tag.div class: 'mediaelement-player' do
-        tag.audio class: 'mejs__player', width: width, style: 'width:100%;', controls: 'controls', preload: 'none' do
-          tag.source type: 'application/x-mpegURL', src: url
-        end
+    tag.div class: 'mediaelement-player' do
+      tag.audio class: 'mejs__player', width: width, style: 'width:100%;', controls: 'controls', preload: 'none' do
+        tag.source type: 'application/x-mpegURL', src: url
       end
     end
   end
