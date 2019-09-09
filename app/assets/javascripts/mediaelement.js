783ff1518927486b8be80f9282eaa14b1290ac7c
Carla Galarza <cmg2228@columbia.edu>
2019-09-09 12:30:06 -0400

    Attempting to add branding

--- app/assets/javascripts/mediaelement.js
+++ app/assets/javascripts/mediaelement.js
@@ -1,8 +1,26 @@
 
 $(document).ready(function(){
-	$('.mediaelement-player video, .mediaelement-player audio').each(function() {
+  MediaElementPlayer.prototype.buildbranding = function(player, controls, layers, media) {
+    // create the branding button, with Academic Commons logo and link back out to the Academic Commons item page.
+
+    var branding =
+    $('<div class="' + player.options.classPrefix + 'branding-button">' +
+    '<button type="button" title="Columbia Academic Commons" aria-label="Columbia Academic Commons" tabindex="0">Columbia Academic Commons</button></div>')
+    // append it to the toolbar
+    .appendTo(controls)
+    // add a click toggle event
+    .click(function() {
+      if (player.node.dataset.brandLink) {
+        window.open(player.node.dataset.brandLink, '_blank')
+      }
+    });
+  }
+
+
+  $('.mediaelement-player video, .mediaelement-player audio').each(function() {
     $(this).mediaelementplayer({
       customError: '<p>There was an error loading this file.</p>',
+      features: ['playpause', 'current', 'progress', 'duration', 'tracks', 'volume', 'fullscreen', 'branding']
     })
   });
 });
--- app/helpers/asset_helper.rb
+++ app/helpers/asset_helper.rb
@@ -1,25 +1,25 @@
 module AssetHelper
-  def player(document)
+  def player(document, brand_link)
     if document.audio?
-      audio_player document.wowza_media_url(request)
+      audio_player document.wowza_media_url(request), brand_link
     elsif document.video?
-      video_player document.wowza_media_url(request), document.image_url(768)
+      video_player document.wowza_media_url(request), document.image_url(768), brand_link
     else
       tag.div 'Not a playable asset'
     end
   end
 
-  def video_player(url, poster_path)
+  def video_player(url, poster_path, brand_link)
     tag.div class: 'mediaelement-player' do
-      tag.video width: '100%', height: '100%', style: 'width:100%;height:100%;', poster: poster_path, controls: 'controls', preload: 'none' do
+      tag.video width: '100%', height: '100%', style: 'width:100%;height:100%;', poster: poster_path, controls: 'controls', preload: 'none', data: { brand_link: brand_link } do
         tag.source type: 'application/x-mpegURL', src: url
       end
     end
   end
 
-  def audio_player(url)
+  def audio_player(url, brand_link)
     tag.div class: 'mediaelement-player' do
-      tag.audio style: 'width:100%;', controls: 'controls', preload: 'none' do
+      tag.audio style: 'width:100%;', controls: 'controls', preload: 'none', data: { brand_link: brand_link } do
         tag.source type: 'application/x-mpegURL', src: url
       end
     end
