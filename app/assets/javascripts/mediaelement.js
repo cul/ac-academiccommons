d07ee9c4c9198ad680d89392d63f4c940f20413a
Ed Madrid <edmadrid@gmail.com>
2019-09-26 12:12:58 -0400

    adds media player branding image

--- app/assets/javascripts/mediaelement.js
+++ app/assets/javascripts/mediaelement.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
     // create the branding button, with Academic Commons logo and link back out to the Academic Commons item page.
 
     var branding =
-    $('<div class="' + player.options.classPrefix + 'branding-button">' +
-    '<button type="button" title="Columbia Academic Commons" aria-label="Columbia Academic Commons" tabindex="0">Columbia Academic Commons</button></div>')
+    $('<div class="' + player.options.classPrefix + 'branding-button" style="width: 200px; cursor:pointer; position: relative;>' +
+    '<button type="button" title="Columbia Academic Commons" aria-label="Columbia Academic Commons" tabindex="0"><img src="/assets/logo-media-player-badge.png" alt="" style="bottom: 9px; height: auto; max-width: 100%; position: absolute; width: 100%;"></button></div>')
     // append it to the toolbar
     .appendTo(controls)
     // add a click toggle event
