4338d28b0f8e63b41ba3be915abe5116ae2a2e17
Ed Madrid <edmadrid@gmail.com>
2019-11-01 16:51:53 -0400

    fixes error in item page test

--- spec/features/item_page_spec.rb
+++ spec/features/item_page_spec.rb
@@ -22,7 +22,7 @@ describe 'Item Page', type: :feature do
   end
 
   it 'has doi' do
-    expect(page).to have_field('#doi', with: '10.7916/ALICE')
+    expect(page).to have_field('doi', with: '10.7916/ALICE')
   end
 
   it 'has journal title' do
