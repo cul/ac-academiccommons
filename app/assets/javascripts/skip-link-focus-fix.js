d4de82790833e49d4e281d20ea19ffba63034e74
Ed Madrid <edmadrid@gmail.com>
2020-02-25 11:48:52 -0500

    adds description for skip-link js

--- app/assets/javascripts/skip-link-focus-fix.js
+++ app/assets/javascripts/skip-link-focus-fix.js
@@ -1,3 +1,5 @@
+// This enables users to navigate to a page's main content using a 'skip to main content' link. This added functionality is necessary because some browsers do not fully support in-page links. While they may visually shift focus to the location of the target or named anchor for the 'skip' link, they do not actually set keyboard focus to this location.
+
 $( document ).ready(function() {
   $(".skip-link").click(function(event){
 
