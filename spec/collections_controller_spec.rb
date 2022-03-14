3fd66c2f077e7221ed206d13b54b17eaee4cf108
Esther Jackson <ej2432@columbia.edu>
2022-03-14 14:19:35 -0400

    ACHYDRA-867 includes link_to routes for featured partner page and the start of a test that is not working yet

---
+++ spec/collections_controller_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe CollectionsController, type: :routing do
+  describe 'routing' do
+    it routes to 'featured partners collection' do
+      expect(get: '/explore/featured').to route_to(controller: 'collections', category_id: 'featured')
+    end
+  end
+end
