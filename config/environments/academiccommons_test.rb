ee2a39907f6596b890729560a4afb3d82c82c417
Eric O <eric@ejdigitalmedia.com>
2025-05-27 12:53:02 -0400

    ACHYDRA-982: Need to whitelist 127.0.0.1 to support X-Forwarded-For parsing for 127.0.0.1 internal proxies

--- config/environments/academiccommons_test.rb
+++ config/environments/academiccommons_test.rb
@@ -12,6 +12,12 @@ AcademicCommons::Application.configure do
   # Setting host so that url helpers can be used in mailer views.
   config.action_mailer.default_url_options = { host: 'academiccommons-test.library.columbia.edu' }
 
+  config.action_dispatch.trusted_proxies = [
+    # Add 127.0.0.1 as a trusted proxy so that the X-Forwarded-For value set by Anubis (or any other internal proxy)
+    # is whitelisted for use by the request.remote_ip IP-determining mechanism.
+    IPAddr.new('127.0.0.1')
+  ]
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
