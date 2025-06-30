96f6d40cee45c2d8921d0555fe36ee40848341fa
goldsmithb <bg2918@columbia.edu>
2025-06-30 15:43:20 -0400

    remove logs, add test

--- app/controllers/admin/downloads_controller.rb
+++ app/controllers/admin/downloads_controller.rb
@@ -5,9 +5,7 @@ module Admin
     authorize_resource class: false
 
     def index
-      Rails.logger.debug ' ahoy there! entered downloads controller index action!'
       @enabled = downloads_enabled?
-      Rails.logger.debug ' ahoy there! trying to render views/admin/downloads/index.html'
     end
   end
 end
