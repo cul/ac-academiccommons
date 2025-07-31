81331e292023282057ab2362d18c10d321b8f753
goldsmithb <bg2918@columbia.edu>
2025-07-31 11:19:38 -0400

    fix migration

--- spec/models/site_configuration_spec.rb
+++ spec/models/site_configuration_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe SiteConfiguration, type: :model, focus: true do # rubocop:disable
 
   describe 'should be valid' do
     it 'with singleton_guard, downloads_enabled, and deposits_enabled present' do
-      expect(test_site_configuration.errors).to be_valid
+      expect(test_site_configuration).to be_valid
     end
 
     it 'with downloads_message and alert_message not present' do
@@ -17,10 +17,22 @@ RSpec.describe SiteConfiguration, type: :model, focus: true do # rubocop:disable
     end
   end
 
-   describe 'should not be valid' do
-     it 'without a singleton_guard field' do
-     end
-   end
+  describe 'should not be valid' do
+    it 'when singleton_guard is nil' do
+      test_site_configuration.singleton_guard = nil
+      expect(test_site_configuration).not_to be_valid
+    end
+
+    it 'when downloads_enabled is nil' do
+      test_site_configuration.downloads_enabled = nil
+      expect(test_site_configuration).not_to be_valid
+    end
+
+    it 'when deposits_enabled is nil' do
+      test_site_configuration.deposits_enabled = nil
+      expect(test_site_configuration).not_to be_valid
+    end
+  end
 
   # describe '#instance' do
   #   it 'creates the instance if it does not exist' do
