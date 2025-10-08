cdca01bc987a43fcd8e977611a958fb2d9e83264
goldsmithb <bg2918@columbia.edu>
2025-10-08 15:55:54 -0400

    debug logs

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -13,14 +13,21 @@ SitemapGenerator::Sitemap.create do
   #           :lastmod => Time.now, :host => default_host
 
   # Add static pages
+  puts 'Inside sitemap create block!'
   add about_path,      priority: 0.7, changefreq: 'monthly'
+    puts "     adding #{about_path}"
   add policies_path,   priority: 0.7, changefreq: 'monthly'
+    puts "     adding #{policies_path}"
   add faq_path,        priority: 0.7, changefreq: 'monthly'
+    puts "     adding #{faq_path}"
   add developers_path, priority: 0.7, changefreq: 'monthly'
+    puts "     adding #{developers_path}"
 
   # Add all items
   items = AcademicCommons.search { |p| p.aggregators_only.field_list('id', 'record_change_dtsi') }.docs
+  puts "Adding #{items.length} items"
   items.each do |item|
+    puts "     adding #{solr_document_path(item.id)}"
     add solr_document_path(item.id), priority: 0.5, changefreq: 'yearly', lastmod: item['record_change_dtsi']
   end
 end
