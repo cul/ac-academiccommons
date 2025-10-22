73b172ee2969319be60c2d63c8aaf985d5161854
Benjamin Armintor <armintor@gmail.com>
2025-10-22 16:05:17 -0400

    wip mcp service based on search api

---
+++ app/tools/records_tool.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class RecordsTool < ApplicationTool
+  description 'Find Works in Academic Commons'
+
+  # Optional: Add annotations to provide hints about the tool's behavior
+  annotations(
+    title: 'Academic Commons Works Search',
+    read_only_hint: true,      # This tool only reads data
+    open_world_hint: false     # This tool only accesses the local database
+  )
+
+  arguments do
+    optional(:search_type)
+      .filled(Dry::Types['string'].default('keyword')).value(included_in?: V1::Helpers::Solr::SEARCH_TYPES.map(&:to_s))
+      .description('type of search to be conducted, in most cases a keyword search should be sufficient')
+    optional(:q)
+      .maybe(:string).description('query string')
+    optional(:page)
+      .filled(Dry::Types['integer'].default(1)).value(gt?: 0).description('page number')
+    optional(:per_page)
+      .filled(Dry::Types['integer'].default(25)).value(included_in?: 1..100)
+      .description('number of results returned per page; the maximum number of results is 100')
+    optional(:sort)
+      .filled(Dry::Types['string'].default('best_match')).value(included_in?: V1::Helpers::Solr::SORT.map(&:to_s))
+      .description('sorting of search results')
+    optional(:order)
+      .filled(Dry::Types['string'].default('desc')).value(included_in?: V1::Helpers::Solr::ORDER.map(&:to_s))
+      .description('ordering of results')
+  end
+
+  def call(order: :desc, page: 1, per_page: 25, q: nil, search_type: :keyword, sort: :best_match)
+    solr_response = query_solr(
+      order: order.to_sym, page: page, per_page: per_page,
+      q: q, search_type: search_type.to_sym, sort: sort.to_sym
+    )
+    params = { page: page, per_page: per_page } # backwards compat with Grape API
+    result_obj = V1::Entities::SearchResponse.represent(solr_response, params: params).as_json
+    # not serializing params or facets for MCP responses for demo
+    result_obj.delete(:params)
+    result_obj.delete(:facets)
+    JSON.generate(result_obj)
+  end
+end
--- config/initializers/fast_mcp.rb
+++ config/initializers/fast_mcp.rb
@@ -13,24 +13,56 @@
 # Mount the MCP middleware in your Rails application
 # You can customize the options below to fit your needs.
 require 'fast_mcp'
+require_relative '../../lib/academic_commons/fast_mcp/transports/managed_tokens_transport'
 
-FastMcp.mount_in_rails(
+def mount_in_rails(app, options = {})
+  # Default options
+  name = options.delete(:name) || app.class.module_parent_name.underscore.dasherize
+  version = options.delete(:version) || '1.0.0'
+  logger = options[:logger] || Rails.logger
+  path_prefix = options.delete(:path_prefix) || '/mcp'
+  messages_route = options.delete(:messages_route) || 'messages'
+  sse_route = options.delete(:sse_route) || 'sse'
+  authenticate = options.delete(:authenticate) || false
+  allowed_origins = options[:allowed_origins] || FastMcp.default_rails_allowed_origins(app)
+  allowed_ips = options[:allowed_ips] || FastMcp::Transports::RackTransport::DEFAULT_ALLOWED_IPS
+
+  options[:localhost_only] = Rails.env.development? if options[:localhost_only].nil?
+  options[:allowed_ips] = allowed_ips
+  options[:logger] = logger
+  options[:allowed_origins] = allowed_origins
+
+  # Create or get the server
+  FastMcp.server = FastMcp::Server.new(name: name, version: version, logger: logger)
+  yield FastMcp.server if block_given?
+
+  # Choose the right middleware based on authentication
+  FastMcp.server.transport_klass =
+    authenticate ? AcademicCommons::FastMcp::Transports::ManagedTokensTransport : FastMcp::Transports::RackTransport
+
+  # Insert the middleware in the Rails middleware stack
+  app.middleware.use(
+    FastMcp.server.transport_klass,
+    FastMcp.server,
+    options.merge(path_prefix: path_prefix, messages_route: messages_route, sse_route: sse_route)
+  )
+end
+
+mount_in_rails(
   Rails.application,
   name: 'cul-academic-commons',
   version: '1.0.0',
   path_prefix: '/mcp', # This is the default path prefix
   messages_route: 'messages', # This is the default route for the messages endpoint
-  sse_route: 'sse' # This is the default route for the SSE endpoint
+  sse_route: 'sse', # This is the default route for the SSE endpoint
   # Add allowed origins below, it defaults to Rails.application.config.hosts
   # allowed_origins: ['localhost', '127.0.0.1', '[::1]', 'example.com', /.*\.example\.com/],
   # localhost_only: true, # Set to false to allow connections from other hosts
   # whitelist specific ips to if you want to run on localhost and allow connections from other IPs
   # allowed_ips: ['127.0.0.1', '::1'],
-  # authenticate: true,       # Uncomment to enable authentication
-  # auth_token: 'your-token', # Required if authenticate: true
+  authenticate: true
 ) do |server|
   Rails.application.config.after_initialize do
-    server.register_tool(WorksTool)
-    server.register_resource(WorkResource)
+    server.register_tool(RecordsTool)
   end
 end
