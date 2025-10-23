969976f10334a8c8817a9cad69cd4277e9815b2f
Benjamin Armintor <armintor@gmail.com>
2025-10-23 00:07:55 -0400

    permit requests from own origin

--- config/environments/academiccommons_prod.rb
+++ config/environments/academiccommons_prod.rb
@@ -1,1 +1,10 @@
 require Rails.root.join('config/environments/deployed')
+
+# WIP: Options that differed from default config (from fast mcp branch)
+
+# TODO: These were commented out (proabbly do not need, based on comments in default config)
+config.assets.js_compressor = Uglifier.new(harmony: true)
+config.assets.css_compressor = :sass
+
+# TODO: Likely keep
+config.hosts << 'academiccommons.columbia.edu'
