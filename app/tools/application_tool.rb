c93b687612673083a2fe3f70e11d5448de812f4c
Benjamin Armintor <armintor@gmail.com>
2025-12-01 18:56:10 -0500

    wip permit semantic search in mcp tool

--- app/tools/application_tool.rb
+++ app/tools/application_tool.rb
@@ -1,29 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationTool < ActionTool::Base
-  SEARCH_TYPES = %i[keyword title subject].freeze
-  SORT    = %i[best_match date title].freeze
-  ORDER   = %i[desc asc].freeze
-  FACETS  = %i[author date department subject type columbia_series].freeze
+  SEARCH_TYPES = V1::Helpers::Solr::SEARCH_TYPES
+  SORT    = V1::Helpers::Solr::SORT
+  ORDER   = V1::Helpers::Solr::ORDER
+  FACETS  = V1::Helpers::Solr::FACETS
 
-  SORT_TO_SOLR_SORT = {
-    best_match: {
-      asc: 'score desc, pub_date_isi desc, title_sort asc',
-      desc: 'score desc, pub_date_isi desc, title_sort asc'
-    },
-    date: {
-      asc: 'pub_date_isi asc, title_sort asc',
-      desc: 'pub_date_isi desc, title_sort asc'
-    },
-    title: {
-      asc: 'title_sort asc, pub_date_isi desc',
-      desc: 'title_sort desc, pub_date_isi desc'
-    }
-  }.freeze
+  SORT_TO_SOLR_SORT = V1::Helpers::Solr::SORT_TO_SOLR_SORT
 
   MAP_TO_SOLR_FIELD = SolrDocument.field_semantics
 
-  def query_solr(order:, page:, per_page:, q:, search_type:, sort:)
+  def query_solr(order:, page:, per_page:, q:, sort:, search_type: :semantic)
     AcademicCommons.search do |solr_params|
       solr_params.q q
       solr_params.sort_by SORT_TO_SOLR_SORT.dig(sort, order)
@@ -31,7 +18,7 @@ class ApplicationTool < ActionTool::Base
       solr_params.rows per_page.to_i
       solr_params.aggregators_with_assets
 
-      solr_params.search_type(search_type) if search_type
+      solr_params.search_type(search_type)
     end
   rescue StandardError => e
     Rails.logger.error e
