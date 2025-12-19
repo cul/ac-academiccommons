05a94f30e7d0e87819b6e094f12a928cda6617ca
goldsmithb <bg2918@columbia.edu>
2025-12-19 11:54:41 -0500

    add back option

--- config/environments/academiccommons_prod.rb
+++ config/environments/academiccommons_prod.rb
@@ -20,7 +20,7 @@ AcademicCommons::Application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # Compress CSS
   # config.assets.css_compressor = :sass
 
