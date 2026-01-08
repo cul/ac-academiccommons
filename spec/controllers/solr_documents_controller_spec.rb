fa889c14ec7b56bc120408c7c5ab95f93b16e2d5
goldsmithb <bg2918@columbia.edu>
2026-01-08 11:32:44 -0500

    mock LDAP service in tests

--- spec/controllers/solr_documents_controller_spec.rb
+++ spec/controllers/solr_documents_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
-describe SolrDocumentsController, type: :controller do
+describe SolrDocumentsController, type: :controller, focus: true do # rubocop:disable RSpec/Focus
+  include_context 'mock ldap request'
+
   shared_context 'good api key' do
     let(:api_key) do
       key = Rails.application.credentials.index_api_key
--- spec/support/ldap_mock.rb
+++ spec/support/ldap_mock.rb
@@ -1,5 +1,15 @@
 shared_context 'mock ldap request' do
+  # This file sets up basic mocking for tests that interact with the cul-ldap gem's
+  # Cul::LDAP library (primarily AC uses its find_by_uni method).
+  #
+  # provides doubles: uni : str, cul_ldap : Cul::LDAP, and cul_ldap_entry : Cul::LDAP::Entry
+  # mocks calls to initialize and find_by_uni
+
   let(:uni) { 'tu123' } # Matches the first uid created by User Factory as in "FactoryBot.create(:deposit, :with_user)"
+
+  let(:cul_ldap) do
+    instance_double('Cul::LDAP')
+  end
 
   let(:cul_ldap_entry) do
     instance_double(
@@ -10,6 +20,7 @@ shared_context 'mock ldap request' do
   end
 
   before :each do
+    allow_any_instance_of(Cul::LDAP).to receive(:initialize).and_return(cul_ldap)
     allow_any_instance_of(Cul::LDAP).to receive(:find_by_uni).with(uni).and_return(cul_ldap_entry)
   end
 end
