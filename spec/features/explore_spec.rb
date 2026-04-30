8ad67e2bf09c87942ad3d445e1f061eeda3c16a4
goldsmithb <bg2918@columbia.edu>
2026-04-30 17:59:28 -0400

    fix tests

--- spec/features/explore_spec.rb
+++ spec/features/explore_spec.rb
@@ -8,12 +8,17 @@ describe 'Explore', type: :feature do
     expect(page).to have_css('div.cul-banner', text: 'Columbia University Libraries')
   end
 
-  CollectionsController::CONFIG.each do |category_id, opts|
+  displayed_collections = CollectionsController::CONFIG.reject { |category_id| category_id == :produced_at_columbia }
+  displayed_collections.each do |category_id, opts|
     it "links to the #{opts[:title]} page" do
       # this needs to look for a link by href
-      expect(page).to have_css("a[href=\"/explore/#{category_id}\"]")
-      find(:css, "a[href=\"/explore/#{category_id}\"]").click
+      expect(page).to have_css("a[href=\"/explore/#{category_id.to_s.tr('_', '-')}\"]")
+      find(:css, "a[href=\"/explore/#{category_id.to_s.tr('_', '-')}\"]").click
       expect(page).to have_css 'h1', text: opts[:title]
     end
+  end
+
+  it 'does not link to the Produced at Columbia page' do
+    expect(page).not_to have_css('a[href="/explore/produced-at-columbia"]')
   end
 end
