359af53d62f5ed7edf3c68ea275d2fa9512497aa
goldsmithb <bg2918@columbia.edu>
2026-05-04 10:32:28 -0400

    upate specs and paths

--- spec/routing/collections_controller_spec.rb
+++ spec/routing/collections_controller_spec.rb
@@ -6,31 +6,31 @@ describe CollectionsController, type: :routing do
   describe 'routing' do
     it 'routes to featured partners collection' do
       expect(get: '/explore/featured-partners').to route_to(
-        controller: 'collections', category_id: 'featured-partners', action: 'show'
+        controller: 'collections', category_slug: 'featured-partners', action: 'show'
       )
     end
 
     it 'routes to doctoral theses collection' do
       expect(get: '/explore/doctoral-theses').to route_to(
-        controller: 'collections', category_id: 'doctoral-theses', action: 'show'
+        controller: 'collections', category_slug: 'doctoral-theses', action: 'show'
       )
     end
 
     it 'routes to produced at columbia collection' do
       expect(get: '/explore/produced-at-columbia').to route_to(
-        controller: 'collections', category_id: 'produced-at-columbia', action: 'show'
+        controller: 'collections', category_slug: 'produced-at-columbia', action: 'show'
       )
     end
 
     it 'routes to featured series collection' do
       expect(get: '/explore/featured-series').to route_to(
-        controller: 'collections', category_id: 'featured-series', action: 'show'
+        controller: 'collections', category_slug: 'featured-series', action: 'show'
       )
     end
 
     it 'routes to columbia journals collection' do
       expect(get: '/explore/columbia-journals').to route_to(
-        controller: 'collections', category_id: 'columbia-journals', action: 'show'
+        controller: 'collections', category_slug: 'columbia-journals', action: 'show'
       )
     end
   end
