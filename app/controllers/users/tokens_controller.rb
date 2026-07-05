a685210085b0088539fa50c172cbf918984645df
goldsmithb <bg2918@columbia.edu>
2026-07-05 15:40:34 -0400

    allow users to refresh MCP tokens

--- app/controllers/users/tokens_controller.rb
+++ app/controllers/users/tokens_controller.rb
@@ -16,34 +16,52 @@ class Users::TokensController < ApplicationController
     end
   end
 
-  def flash_message(token, as_of)
-    if token.persisted?
-      newly_created = as_of <= token.created_at
-      newly_created ? 'Successfully created API token.' : 'Request help to refresh API token.'
-    else
-      token.errors.full_messages.to_sentence
-    end
-  end
+  def update
+    old_token = current_user_token
+    old_token.destroy
 
-  def set_flash!(as_of, token)
-    flash_type = token.persisted? ? :success : :error
-    flash_msg = flash_message(token, as_of)
-    flash[flash_type] = flash_msg
+    new_token = current_user_token
+    http_status = new_token.persisted? ? 200 : 500
+
+    respond_to do |f|
+      f.html do
+        set_flash!(new_token, :update)
+        redirect_to account_path
+      end
+      f.json { render json: { message: flash_message(token, :update) }, http_status: http_status }
+    end
   end
 
   # Right now, we create an MCP scoped token by default!
   def create
-    as_of = DateTime.now
-
     token = current_user_token
     http_status = token.persisted? ? 200 : 500
 
     respond_to do |f|
       f.html do
-        set_flash!(as_of, token)
+        set_flash!(token, :create)
         redirect_to account_path
       end
-      f.json { render json: { message: flash_message(token, as_of) }.to_json, status: http_status }
+      f.json { render json: { message: flash_message(token, :create) }, status: http_status }
     end
+  end
+
+  private
+
+  def flash_message(token, action)
+    return token.errors.full_messages.to_sentence unless token.persisted?
+
+    case action
+    when :create
+      'Successfully created API token.'
+    when :update
+      'Successfully refreshed API token.'
+    end
+  end
+
+  def set_flash!(token, action)
+    flash_type = token.persisted? ? :success : :error
+    message = flash_message(token, action)
+    flash[flash_type] = message
   end
 end
