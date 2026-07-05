4d5ee0061631973f5bfe59ebe8445f82b66eb8c8
goldsmithb <bg2918@columbia.edu>
2026-07-05 14:38:55 -0400

    allow admin to delete tokens

--- app/controllers/admin/tokens_controller.rb
+++ app/controllers/admin/tokens_controller.rb
@@ -5,5 +5,14 @@ module Admin
     load_and_authorize_resource
 
     def index; end
+
+    def destroy
+      Rails.logger.debug "DESTROY TOKEN: #{@token}"
+      @token.destroy!
+      flash[:success] = 'Token deleted'
+      redirect_to action: :index
+    rescue StandardError
+      flash[:error] = 'There was an error deleting this token'
+    end
   end
 end
