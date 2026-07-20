d7917a2f718eb4ab44afc64f2ce5309d202b1ac4
goldsmithb <bg2918@columbia.edu>
2026-07-20 17:08:49 -0400

    fix migrations

--- db/migrate/20251020183031_create_api_clients.rb
+++ db/migrate/20251020183031_create_api_clients.rb
@@ -7,13 +7,5 @@ class CreateAPIClients < ActiveRecord::Migration[7.1]
       t.datetime :created_at
       t.datetime :updated_at
     end
-    Token.all do |token|
-      next if token.authorizable
-      api_client = APIClient.create(
-        contact_email: token.contact_email, name: 'Datafeed API Client', description: token.description
-      )
-      token.authorizable = api_client
-      token.save
-    end
   end
 end
