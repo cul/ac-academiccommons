dfee54441ff31f05c378b81b2f6b53c33f78fa8b
goldsmithb <bg2918@columbia.edu>
2026-08-20 17:03:31 -0400

    fix bug in mcp error logging

--- config/initializers/mcp.rb
+++ config/initializers/mcp.rb
@@ -2,8 +2,8 @@
 
 MCP.configure do |config|
   config.exception_reporter = lambda { |exception, server_context|
-    Rails.logger.error("[MCP]: #{exception.class}: #{exception.message}",
-                       context: server_context,
-                       backtrace: exception.backtrace)
+    Rails.logger.error({ mcp_error: "#{exception.class}: #{exception.message}",
+                         context: server_context,
+                         backtrace: exception.backtrace }.to_json)
   }
 end
